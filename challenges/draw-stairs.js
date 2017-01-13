d6c77b9d414c84a968a2f024f45c41f64797c9f6
Matthias Wagner <matzewagner@gmail.com>
2017-01-13 09:49:38 -0800

    first commit, working but needs refactoring

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,24 @@
 */
 
 function drawStairs(n) {
-
+	const max = n;
+	for (let i = 1; i <= max; i += 1) {
+		let level = "";
+		let empty = max-i;
+		let stars = i;
+		while (empty > 0) {
+			level += " ";
+			empty -= 1;
+		}
+		while (stars > 0) {
+			level += "*";
+			stars -= 1;
+		}
+		console.log(level);
+	}
 }
 
 
 module.exports = drawStairs;
+
+drawStairs(9);
