3fba08ca33de4c64a18dc34d69e50bc3b8695071
Schno Mozingo <schno.mozingo@gmail.com>
2017-01-14 11:05:54 -0800

    Done with mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,42 @@
 
 
 function modemean(array) {
+    let sum = 0;
+    let modes = {};
+    let mode;
+    let max = 1;
 
+
+    for (let i = 0; i < array.length ; i++) {
+        let val = array[i];
+        sum += val;
+        if (modes[val] === undefined ) modes[val] = 0;
+        modes[val] += 1;
+    }
+
+    let mean = Math.floor(sum / array.length);
+
+    for (val in modes) {
+
+            if ( modes[val] > max ) {
+                max = modes[val];
+                mode = val;
+            }
+            else if ( modes[val] === max && val > mode ) {
+                mode = val;
+            }
+
+
+        if(mode === undefined) {
+            mode = Math.max(...Object.keys(modes));
+        }
+
+    }
+
+    console.log("Mean:",mean,"max",mode);
+    return mean === mode;
 }
 
+
+modemean([1,2,3,4,5,2,5,3,3]);
 module.exports = modemean;
