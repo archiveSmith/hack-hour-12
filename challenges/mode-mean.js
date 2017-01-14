fc3e29e9bb313c2f759f7841158cd8c395cd22b0
Trung Dinh <trungdinh0429@gmail.com>
2017-01-14 10:01:33 -0800

    mode-mean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -8,10 +8,28 @@
  * Return true or false
  *
  */
-
-
 function modemean(array) {
-  console.log(2)
+  let obj = {};
+  let max = 0;
+  let mode;
+  let sum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+      //mean;
+      sum += array[i];
+      //mode;
+      if (obj[array[i]] === undefined) {
+          obj[array[i]] = 1;
+      } else {
+          obj[array[i]]++;
+          if (obj[array[i]] > max) {
+            mode = array[i];
+            max = obj[mode];
+          }
+      }
+  }
+
+  return mode === Math.floor(sum/array.length);
 }
 
 module.exports = modemean;
