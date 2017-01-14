812f91e7790b27d0771879971d7d64260c2cecfd
Joseph Vu <jvu009@ucr.edu>
2017-01-14 10:05:45 -0800

    first test

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,30 @@
  */
 
 
-function modemean(array) {
+ function modemean(array) {
+   let mean = 0;
+   let mode = array[0];
+   const cache = {};
 
-}
+   for (let i = 0; i < array.length; i++) {
+     mean += array[i];
+     if (!cache[array[i]]) cache[array[i]] = 1;
+     else cache[array[i]] += 1;
+   }
+
+   mean = Math.floor(mean / array.length);
+
+   for (let key in cache) {
+     if (cache[key] > cache[mode]) {
+       mode = key;
+     } else if (cache[key] === cache[mode]) {
+       if (key > mode) {
+         mode = key;
+       }
+     }
+   }
+
+   return mean === mode;
+ }
 
 module.exports = modemean;
