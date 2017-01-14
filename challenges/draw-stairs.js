fef3c8469d4777ff059720d1e1954775e4cb5a12
Kyle Shaw <kyleshaw@Kyles-MacBook-Pro.local>
2017-01-14 09:21:18 -0800

    second hack hour

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -12,9 +12,17 @@
 ******
 
 */
+// for each asterisks there should be equal amounts of leading opposite whitespaces
+// init an empty string to store values
+// store leading whitespaces n - 1 plus asterisks
+// add new line after each iteration
+
+
 
 function drawStairs(n) {
-
+  for (let i = 1; i < n; i+=1) {
+    ' '.repeat(n - i) + '*'.repeat(i);
+  }
 }
 
 
