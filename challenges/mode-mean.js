d6e8ba49fe3b0d14b1d90cd49644e746a9a031fa
Brandon Smith <bxleesmith@gmail.com>
2017-01-14 10:51:13 -0800

    finished mode mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,24 @@
 
 
 function modemean(array) {
-  
+  array = array.sort((a, b) => a - b);
+  let sum = 0;
+  let cache = {};
+  for (let i = 0; i < array.length; i += 1) {
+    sum += array[i];
+    if (cache[array[i]]) cache[array[i]] += 1;
+    else cache[array[i]] = 1;
+  }
+
+  let mode = array[0];
+  for (let prop in cache) {
+    if (cache[prop] > cache[mode]) mode = prop;
+    else if (cache[prop] === cache[mode]) {
+      mode = prop < mode ? mode : prop;
+    } 
+  }
+
+  return Number(mode) === Math.floor(sum/array.length);
 }
 
 module.exports = modemean;
