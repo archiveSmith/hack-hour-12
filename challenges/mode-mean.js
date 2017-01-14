6406eabe08dacf3efb4b171368f9d430ee643acb
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-01-14 10:03:48 -0800

    MeanMode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,6 +11,34 @@
 
 
 function modemean(array) {
+	let mean = 0;
+
+	for (var i = 0; i < array.length; i++) {
+		mean += array[i];
+	}
+	mean = mean / array.length;
+
+	// get mode
+	let mode;
+	let modes = [];
+	let modeCount = {};
+
+	// use modeCount to increment counts
+	for (var i = 0; i < array.length; i++) {
+		if (!modeCount[array[i]]) modeCount[array[i]] = 0;
+		modeCount[array[i]] += 1;
+	}
+
+	for (num in modeCount) {
+		if (modeCount[num] >= mode || !mode) {
+			mode = num;
+			modes.push(num);
+		}
+	}
+
+	if (Math.max(modes) > mode) mode = Math.max(modes);
+
+	return mode === mean;
 
 }
 
