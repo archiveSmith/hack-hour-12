cc3312a44e2854e3b327afd45010a8f8c61ef8ab
Joseph Vu <jvu009@ucr.edu>
2017-01-16 07:51:12 -0800

    update fizzbuzz and draw-stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -14,8 +14,9 @@
 */
 
 function drawStairs(n) {
-
+  for (let i = 0; i < n; i++) {
+    console.log(' '.repeat(n-i-1) + '*'.repeat(i+1));
+  }
 }
-
 
 module.exports = drawStairs;
