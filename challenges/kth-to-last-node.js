16f1c8c82fea0b7387247a140426fc5a01c52d89
shahrodkh <shahrodkh@gmail.com>
2017-01-17 09:37:34 -0800

    completed hack hour

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -2,18 +2,18 @@
  * Write a function that takes an integer and the head of a singly linked list,
  * and returns the VALUE kth to last node in the list.
  *
- * var a = new Node('A');
- * var b = new Node('B');
- * var c = new Node('C');
- * var d = new Node('D');
- * var e = new Node('E');
- *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
- *
- * kthToLastNode(2,a); -> returns the node with the value 'D' (the second to last node)
+  var a = new Node('A');
+  var b = new Node('B');
+  var c = new Node('C');
+  var d = new Node('D');
+  var e = new Node('E');
+ 
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  d.next = e;
+ 
+  kthToLastNode(2,a); -> returns the node with the value 'D' (the second to last node)
  */
 
 function Node(val) {
@@ -22,7 +22,30 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  let listLength = 0;
+  let currNode = head;
+  do {
+    currNode = currNode.next;
+    listLength += 1;
+  } while (currNode);
+  currNode = head;
+  for (let i = 0; i < listLength - k; i += 1) {
+    currNode = currNode.next;
+  }
+  return currNode;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+  var a = new Node('A');
+  var b = new Node('B');
+  var c = new Node('C');
+  var d = new Node('D');
+  var e = new Node('E');
+ 
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  d.next = e;
+ 
+  console.log(kthToLastNode(2,a));
