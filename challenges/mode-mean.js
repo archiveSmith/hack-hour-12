dbb87fe560fde70f572e3915766a3d0291046d46
Bita Djaghouri <bita@bitadj.com>
2017-01-16 22:15:19 -0800

    append snake body to dom

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
+  let sum = array.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  let mean = Math.floor(sum / array.length);
 
+  let modeMap = {}
+
+  for(let i = 0; i < array.length; i++){
+    if(!modeMap[array[i]]){
+      modeMap[array[i]] = 0;
+    }
+    modeMap[array[i]]++;
+  }
+
+
+
+  // let mode = Math.max.apply(null, modeMap);
+
+  // console.log(modeMap)
+  
+// console.log (sum)
 }
 
 module.exports = modemean;
