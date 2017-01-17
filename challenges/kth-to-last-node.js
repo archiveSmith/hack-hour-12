27f08f6a41f710ad22008705effc8098be4fb0a5
shahrodkh <shahrodkh@gmail.com>
2017-01-17 09:45:46 -0800

    completed hack hour

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -37,15 +37,15 @@ function kthToLastNode(k, head) {
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
 
-  var a = new Node('A');
-  var b = new Node('B');
-  var c = new Node('C');
-  var d = new Node('D');
-  var e = new Node('E');
+  // var a = new Node('A');
+  // var b = new Node('B');
+  // var c = new Node('C');
+  // var d = new Node('D');
+  // var e = new Node('E');
  
-  a.next = b;
-  b.next = c;
-  c.next = d;
-  d.next = e;
+  // a.next = b;
+  // b.next = c;
+  // c.next = d;
+  // d.next = e;
  
-  console.log(kthToLastNode(10,a));
+  // console.log(kthToLastNode(10,a));
