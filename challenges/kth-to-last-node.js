a4baaa4b41ebea83c180951870dcc34e37ffe9a5
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-01-17 10:11:52 -0800

    finished kth to last

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -2,16 +2,16 @@
  * Write a function that takes an integer and the head of a singly linked list,
  * and returns the VALUE kth to last node in the list.
  *
- * var a = new Node('A');
- * var b = new Node('B');
- * var c = new Node('C');
- * var d = new Node('D');
- * var e = new Node('E');
- *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
+ var a = new Node('A');
+ var b = new Node('B');
+ var c = new Node('C');
+ var d = new Node('D');
+ var e = new Node('E');
+ 
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
  *
  * kthToLastNode(2,a); -> returns the node with the value 'D' (the second to last node)
  */
@@ -22,7 +22,28 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let currentNode = head;
+  let count = 1;
+  
+  //Find the length of the linked list
+  while (currentNode.next !== null) {
+    count += 1;
+    currentNode = currentNode.next;
+  }
 
+  //Check for edge cases
+  if (count < k || k < 0) return undefined;
+
+  //Find how far from the head to traverse, reset the head
+  let numJumps = count - k;
+  currentNode = head;
+
+  //Navigate to the node we want
+  for (let i = 0; i < numJumps; i += 1) {
+    currentNode = currentNode.next;
+  }
+  
+  return currentNode.value;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
