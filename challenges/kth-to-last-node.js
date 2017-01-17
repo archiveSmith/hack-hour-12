3edab0a3a16ad8b0f8878d0f6f31f0b6b32c8e77
Joseph Vu <jvu009@ucr.edu>
2017-01-17 09:38:04 -0800

    run first test

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,38 @@ function Node(val) {
   this.next = null;
 }
 
+// var a = new Node('A');
+// var b = new Node('B');
+// var c = new Node('C');
+// var d = new Node('D');
+// var e = new Node('E');
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
 function kthToLastNode(k, head) {
+  let counter = 1;
+  let curr = head;
+  while (curr.next) {
+    counter++;
+    curr = curr.next;
+  }
+  let goUntil = counter - k;
+
+  counter = 0;
+  curr = head;
+  while (counter++ < goUntil) {
+    curr = curr.next;
+  }
+
+  // console.log(curr);
+
+  return curr.value;
 
 }
+
+// console.log(kthToLastNode(5, a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
