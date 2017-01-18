28d69024b141cb7417bf8ca6619aad5a541546fa
Brandon Smith <bxleesmith@gmail.com>
2017-01-18 10:00:28 -0800

    fixed power for case 0

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,8 +3,11 @@
  */
 
 function pow(base, power) {
+  if (power === 0) return 1;
   if (power === 1) return base;
   return pow(base, Math.floor(power / 2)) * pow(base, Math.round(power / 2));
 }
+
+console.log(pow(2, 1))
 
 module.exports = pow;
