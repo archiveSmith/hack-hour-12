7389b978d053928f9c76b8f6e49958c471fbddb2
hit1st <hit1st4731@sbcglobal.net>
2017-01-18 09:54:20 -0800

    pow submit

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,12 @@
  */
 
 function pow(base, power) {
+  // establish base case, power is the value
+  // that changes
+  if (power === 0) return 1;
 
+  // return call to pow with power - 1
+  return base * pow(base, power - 1);
 }
 
 module.exports = pow;
