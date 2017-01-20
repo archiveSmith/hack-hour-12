b8c30e95d2f40464f3a7850c1d7dfb8c5305cb7d
Trung Dinh <trungdinh0429@gmail.com>
2017-01-20 09:41:04 -0800

    hack hour reverse

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,18 @@
  */
 
 function reverseInPlace(array) {
+  let max = Math.floor(array.length / 2) - 1;
 
+  for (var i = 0; i <= max; i++) {
+      let temp = array[array.length - 1 - i];
+      array[array.length - 1 - i] = array[i];
+      array[i] = temp;
+  }
+
+  return array;
 }
+
+// console.log(reverseInPlace([1,2,3,4,5]))
+// console.log(reverseInPlace([1,2,3,4]))
 
 module.exports = reverseInPlace;
