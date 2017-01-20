4416b1400beb9f106e4eff6c6628183839906ae0
Rodrigo Leme <lemerodrigo@gmail.com>
2017-01-20 09:31:33 -0800

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,7 +4,7 @@
 
 function pow(base, power) {
     if (!base || !power) return;
-    if (power === 0) return 1;
+    if (power === 1) return base;
     return base * pow(base, power -= 1)
 }
 module.exports = pow;
