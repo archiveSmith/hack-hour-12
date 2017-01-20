c34d70f2cb22a661b83d97228d35ac2a34d9adf9
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-01-20 09:52:31 -0800

    reverseInPlace no input case

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,6 +14,7 @@
  */
 
 function reverseInPlace(array) {
+    if (!array) return;
     const ogLength = array.length;
     for (let i = array.length - 1; i > 0; i--) {
         array.push(array[i]);
