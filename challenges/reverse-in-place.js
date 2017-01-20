48f2c73c5ca24cbb802ce9018e1ec66215fe2467
Deep Pulusani <sdeep27@yahoo.com>
2017-01-20 09:39:51 -0800

    finished reverseinplace

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,16 @@
  */
 
 function reverseInPlace(array) {
-
+	function swap(arr, a, b){
+		let temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+	if(!array[0]) return array;
+	for (let i = 0; i < Math.floor(array.length/2); i++) {
+		swap(array, i, array.length-(i+1));
+	}
+	return array;
 }
 
 module.exports = reverseInPlace;
