39aa7b231183437338b7e7070ef84643218d2548
Matthias Wagner <matzewagner@gmail.com>
2017-01-21 09:41:01 -0800

    first commit, does not use is Substring

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -1,3 +1,4 @@
+
 /* Given two strings, write a function to check if the second one is a rotation of the first,
  * using only one call to isSubstring. The function isSubstring takes in two strings and returns
  * true if the second string is found in the first, false otherwise.
@@ -16,7 +17,15 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  if (typeof s1 !== 'string' || typeof s2 !== 'string') return false;
+  if (s1.length !== s2.length) return false;
+  for (let i = 0; i < s2.length; i += 1) {
+    let currRotation = "";
+    for (let j = i; j < s2.length+i; j += 1) {
+      currRotation += s2.charAt(j%s2.length);
+    }
+    if (currRotation === s1) return true;
+  }
+  return false;
 }
-
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
