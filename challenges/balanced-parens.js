5fd7c1a5590b8040d162b7351f109d657c0b982b
Matthias Wagner <matzewagner@gmail.com>
2017-01-24 09:35:18 -0800

    first commit

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,39 @@
  */
 
 function balancedParens(input){
+	if (!input || input.length < 1) return false;
+	const depths = Array(3).fill(0);
+
+	for (let i = 0; i < input.length; i += 1) {
+		let char = input.charAt(i);
+		switch(char) {
+			case '(':
+				depths[0] += 1;
+				break;
+			case ')':
+				depths[0] -= 1;
+				break;
+			case '[':
+				depths[1] += 1;
+				break;
+			case ']':
+				depths[1] -= 1;
+				break;
+			case '{':
+				depths[2] += 1;
+				break;
+			case '}':
+				depths[2] -= 1;
+				break;
+			default:
+				break;
+		}
+	}
+
+	return (depths.reduce((res, acc) => res + acc, 0) === 0);
 
 }
+
+console.log(balancedParens('[(]{)}'));
 
 module.exports = balancedParens;
