748f0c67a1eed4e011d020b6f4dac778ccec9ec6
Matthias Wagner <matzewagner@gmail.com>
2017-01-26 10:02:10 -0800

    first commit for roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -20,26 +20,23 @@
 function romanNumeral(n) {
     let cache = [[1, 'I'],[4, 'IV'],[5, 'V'],[9, 'IX'],[10, 'X'],[40, 'XL'],[50, 'L'],[90, 'XC'],[100, 'C'],[400, 'CD'],[500, 'D'],[900, 'CM'],[1000, 'M']];
     let output = "";
-    
     function recurse(num) {
-        console.log('num at top: ', num);
         if (num === 0) return;
-        if (num === 1) { output += 'I'; return 0;}
         let i = cache.length - 1;
         let target = cache[i][0];
-        while (target >= num) {
+        if (num === target) { output += cache[i][1]; return;}
+        while (target > num) {
             i -= 1;
             target = cache[i][0];
-            console.log('inside: ', target, ' n: ', num,  ' output: ', output);
         }
         output += cache[i][1];
-
-        console.log('outside: ', target, ' n: ', num,  ' next: ', num - target);
-        return recurse(num - target);
+        // console.log('outside: ', target, ' n: ', num,  ' next: ', num - target, 'output: ', output);
+        recurse(num - target);
     }
-
    recurse(n);
+   return output;
 }
-
-romanNumeral(36);
+// for (let i = 0; i < 4000; i += 1) {
+//     console.log(romanNumeral(i));
+// }
 module.exports = romanNumeral;
