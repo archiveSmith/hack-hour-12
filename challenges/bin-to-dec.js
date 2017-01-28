a73e6aee055119bbb5e8ac6a1356488d6c94c3bd
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-01-28 10:13:24 -0800

    binToDec sorta

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -5,16 +5,24 @@
  * Example:
 * 	binToDec('0')   -> 0
  * 	binToDec('11')  -> 3
- * 	binToDec('100') -> 4
+ * 	binToDec('100') -> 4 1*0 + 1 + 
  * 	binToDec('101') -> 5
- *  binToDec('0101') -> 5
+ *  binToDec('0101') -> 5 
  *
  * Extension:
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
 function binToDec(binary) {
-
+  const arr = binary.split('').map(a => parseInt(a));
+  while (arr[0] === 0) {
+    arr.shift();
+  }
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + (arr[i] * Math.pow(2, i));
+  }
+	return sum;
 }
 
 module.exports = binToDec;
