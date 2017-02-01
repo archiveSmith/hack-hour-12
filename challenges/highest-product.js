f98e34cd3d08e22fb9c0aaf6c87a2f1992c56a41
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-01 14:34:22 -0800

    largest product revision

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -7,11 +7,19 @@
  // whichever is bigger, multiply by the remaining largst number
 
 function highestProduct(array) {
-  if (array.lengh < 3) return;
+  if (array.lengh < 3) return 0;
+
   array.sort((a, b) => a - b);
+
+  last1 = array[array.length - 1];
+  last2 = array[array.length - 2];
+  last3 = array[array.length - 3];
   lowestTwo = array[0] * array[1];
-  highestTwo = array[array.length - 1] * array[array.length - 2];
-  return lowestTwo > highestTwo ? lowestTwo * array[array.length - 1] : highestTwo * array[array.length - 3];
+  highestTwo = last1 * last2;
+
+  if (last1 < 0) return last1 * last2 * last3;
+
+  return lowestTwo > highestTwo ? lowestTwo * last1 : highestTwo * last3;
 }
 
 module.exports = highestProduct;
