21a1c9b1b3834487ecaea4e9af2619796f1be217
hit1st <hit1st4731@sbcglobal.net>
2017-02-01 09:38:37 -0800

    submit

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,26 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+  if (typeof str !== 'string') return 'not a string';
+  if (str === "") return true;
+  
+  const nonWord = /[^A-Za-z0-9]/;
+  const arr = str.split(nonWord).filter((word) => { return word !== ""; }).map((word) => { return word.toLowerCase(); });
+  const wordCounter = [];  //  counter for matched words
+  const arrLength = arr.length;
+  
+  for (let i = 0; i < arrLength; i += 1) {
+    // if input[i] is an open parens type, push parens type to parensCounter
+    if (wordCounter.length === 0) { wordCounter.push(arr[i]); }
+    else if (arr[i].split("").reverse().join("") === wordCounter[wordCounter.length - 1]) { wordCounter.pop(); }
+  }
+  
+  // if wordCounter === 0 we have matched words
+  if (wordCounter.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = matchWord;
