c361c2d07c4c8e5f2c5edfca721dcfb78eac10db
Curt Toppel <ctoppel@aol.com>
2017-02-01 10:01:16 -0800

    complete

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,15 +3,18 @@
  */
 
  function highestProduct(array) {
+   if (array.length < 3) return 0;
    const sortedArray = array.sort();
    const absSortedArray = array.map(e => Math.abs(e)).sort((a, b) => a - b).reverse();
 
    if (Math.min(...sortedArray.slice(1)) >= 0) {
      sortedArray.reverse();
      return array[0] * array[1] * array[2];
-     // console.log(array)
    }
-   // console.log(absSortedArray);
+   if (Math.max(...sortedArray) < 0) {
+     return absSortedArray[0] * absSortedArray[1] * absSortedArray[absSortedArray.length - 1];
+   }
+
    return absSortedArray[0] * absSortedArray[1] * absSortedArray[2];
  }
 
