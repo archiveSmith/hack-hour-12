31dfd506041cf4edfc31cc2781e617f3d8fbd457
Curt Toppel <ctoppel@aol.com>
2017-02-01 09:56:04 -0800

    complete

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,18 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+ function highestProduct(array) {
+   const sortedArray = array.sort();
+   const absSortedArray = array.map(e => Math.abs(e)).sort((a, b) => a - b).reverse();
 
-}
+   if (Math.min(...sortedArray.slice(1)) >= 0) {
+     sortedArray.reverse();
+     return array[0] * array[1] * array[2];
+     // console.log(array)
+   }
+   // console.log(absSortedArray);
+   return absSortedArray[0] * absSortedArray[1] * absSortedArray[2];
+ }
 
 
 module.exports = highestProduct;
