97c08ae633ce64a9f066df015497eafcb06270ae
Joseph Vu <jvu009@ucr.edu>
2017-02-02 10:00:31 -0800

    buggy

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,74 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (!l1) return l1;
+
+  const newLL = new Node(l1.value);
+  let newCurr = newLL;
+
+  let currLeft = l1;
+  let currRight = l2;
+
+
+  let currTurn = 'right';
+  let currNode = l2;
+
+  while (currNode) {
+    console.log(currNode);
+    const newNode = new Node(currNode.value);
+    newCurr.next = newNode;
+    newCurr = newNode;
+
+    if (currTurn === 'right') {
+      currTurn = 'left';
+      currNode = currLeft.next;
+      currRight = currRight.next;
+      // currLeft = currLeft.next;
+    } else {
+      currTurn = 'right';
+      currNode = currRight.next;
+      currLeft = currLeft.next;
+      // currRight = currRight.next;
+    }
+  }
+
+  // one of the linked lists hit the end
+
+  if (currTurn === 'left') {
+    // left has no more nodes
+    // append right nodes to new linked list
+    while (currRight.next) {
+      const newNode = new Node(currRight.next.value);
+      newCurr.next = newNode;
+      newCurr = newNode;
+      currRight = currRight.next;
+    }
+  } else {
+    while (currLeft.next) {
+      const newNode = new Node(currLeft.next.value);
+      newCurr.next = newNode;
+      newCurr = newNode;
+      currLeft = currLeft.next;
+    }
+  }
+
+  return newLL;
 };
+
+const l1 = new Node(1);
+l1.next = new Node(3);
+l1.next.next = new Node(5);
+
+const l2 = new Node(2);
+l2.next = new Node(4);
+l2.next.next = new Node(6);
+l2.next.next.next = new Node(7);
+
+const l3 = zip(l1, l2);
+let currNode = l3;
+while (currNode) {
+  console.log(currNode.value);
+  currNode = currNode.next;
+}
 
 module.exports = {Node: Node, zip: zip};
