6cd988c8cff5e30334b2d482cc1ab8fc80261c77
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-02-02 08:32:38 -0800

    refactored solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,45 +2,15 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 function highestProduct(array) {
-  console.log(array);
   if (!array || array.length < 3) return 0;
 
-  const positiveArr = array.filter(number => {
-    return number > 0;
-  })  
-  const negativeArr = array.filter(number => {
-    return number < 0;
-  })
+  array.sort();
+  const maxFromMins = array[0] * array[1];
+  const topMax = array[array.length - 2] * array[array.length - 3];
 
-  const sortedPosArr = positiveArr.sort((a, b) => { return b-a });
-  const sortedNegativeArr = negativeArr.sort((a, b) => {return b-a});
-
-  const almostMaxPositive = sortedPosArr[1] * sortedPosArr[2];
-  const minimumPositive = sortedPosArr[sortedPosArr.length - 1] * sortedPosArr[sortedPosArr.length - 2];
-  const maxNegative = sortedNegativeArr[sortedNegativeArr.length - 1] * sortedNegativeArr[sortedNegativeArr.length - 2];
-
-  if (sortedPosArr.length > 2) {
-    if (almostMaxPositive >= maxNegative) {
-      return sortedPosArr[0] * sortedPosArr[1] * sortedPosArr[2];
-    }
-    else {
-      return maxNegative * sortedPosArr[0];
-    }
-  }
-  else if (sortedPosArr.length === 2) {
-    if (sortedNegativeArr.length < 2) {
-      return minimumPositive * sortedNegativeArr[0];
-    }
-    else {
-      return sortedPosArr[0] * maxNegative;
-    }
-  }
-  else if (sortedPosArr.length === 1) {
-    return sortedPosArr[0] * maxNegative;
-  }
-  else {
-    return sortedNegativeArr[0] * sortedNegativeArr[1] * sortedNegativeArr[2];
-  }
+  if (maxFromMins > topMax) return maxFromMins * array[array.length - 1];
+  return topMax * array[array.length - 1];
 }
+
 
 module.exports = highestProduct;
