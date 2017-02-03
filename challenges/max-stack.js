05365e2639784a6d209fe83e40ebeda5d38fdcd1
Deep Pulusani <sdeep27@yahoo.com>
2017-02-03 09:52:12 -0800

    finished max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,31 @@
 
 function Stack() {
   // body...
+  this.values = {};
+  this.max = 0;
+  this.length = 0;
+  this.push = function(value) {
+    if (this.max < value) this.max = value;
+    this.values[this.length] = value;
+    this.length++;
+    return this.length;
+  }
+  this.pop = function(){
+    this.length--;
+    let popped = this.values[this.length];
+    delete this.values[this.length];
+    return popped;
+  }
+  this.getMax = function() {
+    return this.max;
+  }
 }
+const testStack = new Stack();
+console.log(testStack.push(3));
+console.log(testStack.push(8));
+console.log(testStack.pop());
+console.log(testStack.push(10));
+console.log(testStack.getMax());
+
 
 module.exports = Stack;
