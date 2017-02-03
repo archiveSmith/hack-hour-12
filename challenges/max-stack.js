7bb73c78832a02ae983849e70acca3bfaf98a150
Jin Choi <jinchoi@ucdavis.edu>
2017-02-03 17:50:13 +0000

    finished max-stack first version

---
+++ challenges/max-stack.js
@@ -0,0 +1,40 @@
+'use strict'
+/*
+ * Create a stack with the push, pop, and getMax methods.
+ * push should return the new length of the stack.
+ * pop should return the element that was just removed.
+ * getMax should return the largest value currently in the stack.
+ * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
+ */
+
+function Stack() {
+  // body...
+  this.stack = [];
+  
+  this.push = (ele) => {
+      this.stack[this.stack.length] = ele;
+      return this.stack.length;
+  }
+  
+  this.pop = (ele) => {
+      const popped = this.stack[this.stack.length - 1];
+      this.stack.splice(this.stack.length - 1, 1);
+      return popped;
+  }
+  
+  this.getMax = () => {
+      return this.stack.reduce(( a, b ) => {
+          return b > a ? b : a;
+      }, -Infinity)
+  }
+  
+}
+
+// let stack = new Stack();
+// stack.push(5);
+// console.log(stack);
+// console.log(stack.pop());
+// stack.push(4);
+// stack.push(7);
+// console.log(stack.getMax())
+module.exports = Stack;
