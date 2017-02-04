cb26553619e7253a1c20f466d04ebef35993ce3d
Matthias Wagner <matzewagner@gmail.com>
2017-02-04 09:36:18 -0800

    first commit reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,6 +15,36 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 
+    const nodeStack = [head];
+    let curr = head.next;
+    while (curr) {
+        nodeStack.push(curr);
+        console.log('stack from push: ', nodeStack);
+        curr = curr.next;
+    }
+    head = nodeStack.pop();
+    head.next = null;
+    curr = head;
+    while (nodeStack.length > 0) {
+        curr.next = nodeStack.pop();
+        console.log('list from pop: ', head);
+        curr = curr.next;
+    }
+
+    return head;
 }
+
+// let a = new Node(1);
+// let b = new Node(2);
+// let c = new Node(3);
+// let d = new Node(4);
+// let e = new Node(5);
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+// console.log('result: ', reverseLinkedList(a));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
