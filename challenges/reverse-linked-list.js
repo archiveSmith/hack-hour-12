0d69ac81ca7caf18bf90c9330beba8fb146cea74
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-02-04 09:40:37 -0800

    added comments and changed return value for null head

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,20 +15,25 @@ function Node(value) {
 
 function reverseLinkedList(head) {
   // Check for edge cases
-  if (!head) return undefined;
+  if (!head) return null;
   if (head.next === null) return head;
 
+  // Create new variable to not reassign argument
   let newHead = head;
   let i;
   const nodeArr = [];
+  // Add each successive node to the array
   while (newHead.next !== null) {
     nodeArr.unshift(newHead);
     newHead = newHead.next;
   }
+  // Add the tail which will become the head
   nodeArr.unshift(newHead);
+  // Reassign next values for all the nodes
   for (i = 0; i < nodeArr.length - 1; i += 1) {
     nodeArr[i].next = nodeArr[i + 1];
   }
+  // Set the new tail next value
   nodeArr[nodeArr.length - 1].next = null;
   return nodeArr[0];
 }
