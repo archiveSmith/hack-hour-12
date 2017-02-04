677e2e29a41120a6afe9bfd8a4e4b5ea39bce94a
skykeeper0 <trungdinh0429@gmail.com>
2017-02-04 09:40:00 -0800

    reversed linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,32 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+    // edge cases
+        // head == null
+    if (!head) return null;
+        // there only one linked list in head
+    if (!head.next) return head
 
+
+    let arr = [];
+    let currentNode = head;
+    while (currentNode) {
+        arr.push(currentNode.value);
+        currentNode = currentNode.next;
+    }
+
+    let result = new Node(arr[arr.length - 1])
+    let newNode = result;
+    for (let i = arr.length - 2; i >= 0; i--) {
+        newNode.next = new Node(arr[i]);
+        newNode = newNode.next;
+    }
+    return result;
 }
 
+// let start = new Node(1);
+// start.next = new Node(3);
+// start.next.next = new Node(5)
+
+// console.log(reverseLinkedList(start))
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
