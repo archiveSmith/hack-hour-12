1a7a03bbadde8aa0cf8bcfaddc84e0100a98ffe2
Matthias Wagner <matzewagner@gmail.com>
2017-02-03 18:08:34 -0800

    modified getMax again

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -32,7 +32,19 @@ function zip(l1, l2) {
   tempNode.next = l2 ? l2 : l1;
 
   return head; // return head
-};
+
+}
+  /**
+   *  recursive:
+   *  function zip(l1, l2) {
+   *    if (!l1) return l2;
+   *    if (!l2) return l1;
+   * 
+   *    l1. = zip(l2, l1.next);
+   * 
+   *    return l1;  
+   * }
+   */
 
 
 module.exports = {Node: Node, zip: zip};
