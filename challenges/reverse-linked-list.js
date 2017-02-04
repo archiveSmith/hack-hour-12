98814dce90d6e80ee5cf20cc49c6b3bd3c8d7d84
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-04 09:45:53 -0800

    Reversed linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,38 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    if (head === null) return;
+    if (head.next === null) return head;
+    let array = [];
+    let reversed = null;
+    while (head) {
+        array.push(head.value);
+        head = head.next;
+    }
+    for (let i = array.length - 1; i >= 0; i--) {
+      let newNode = new Node(array[i]);
+      if (reversed === null) {
+          reversed = newNode;
+      } else {
+        current = reversed;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+      }
+   }
+   return reversed;
 }
+
+// {"value":1,"next":{"value":2,"next":{"value":3,"next":{"value":4,"next":null}}}
+
+// let l1 = new Node(1);
+// l1.next = new Node(2);
+// l1.next.next = new Node(3);
+// l1.next.next.next = new Node(4);
+// l1.next.next.next.next = new Node(5);
+// console.log(JSON.stringify(l1));
+
+// console.log(reverseLinkedList(l1));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
