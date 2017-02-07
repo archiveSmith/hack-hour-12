d8452a22a3bbf1d91e12d5a58e0e608572b7f88e
Curt Toppel <ctoppel@aol.com>
2017-02-07 10:01:27 -0800

    complete

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,16 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time and O(n) space 
+ *
+ * BONUS:
+ * Complete in O(n) time and O(n) space
  *
  */
 
 function mergeArrays(arr1, arr2) {
-
+  if (!arr1) return arr2;
+  if (!arr2) return arr1;
+  return arr1.concat(arr2).sort((a, b) => b - a);
 }
 
 module.exports = mergeArrays;
