add922eb42303dcffde1b54289724067ff630012
Joseph Vu <jvu009@ucr.edu>
2017-02-08 09:58:16 -0800

    up to ten thousand

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,88 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+const numbers = {
+  '0': 'Zero',
+  '1': 'One',
+  '2': 'Two',
+  '3': 'Three',
+  '4': 'Four',
+  '5': 'Five',
+  '6': 'Six',
+  '7': 'Seven',
+  '8': 'Eight',
+  '9': 'Nine',
+  '10': 'Ten',
+  '11': 'Eleven',
+  '12': 'Twelve',
+  '13': 'Thirteen',
+  '14': 'Fourteen',
+  '15': 'Fifteen',
+  '16': 'Sixteen',
+  '17': 'Seventeen',
+  '18': 'Eighteen',
+  '19': 'Nineteen',
+};
 
+const tens = {
+  '1': 'Ten',
+  '2': 'Twenty',
+  '3': 'Thirty',
+  '4': 'Forty',
+  '5': 'Fifty',
+  '6': 'Sixty',
+  '7': 'Seventy',
+  '8': 'Eighty',
+  '9': 'Ninety',
+};
+
+const places = {
+  '0': '',
+  '1': '',
+  '2': 'Hundred',
+  '3': 'Thousand',
+  '4': 'Thousand',
+};
+
+function numToWords(num) {
+  let str = String(num);
+  const length = str.length;
+
+  if (length === 2) {
+    // teen
+    if (str[0] === '1') return numbers[str];
+
+    // 20-99
+    if (str[1] === '0') return tens[str[0]];
+    return tens[str[0]] + numbers[str[1]];
+  }
+
+  if (length < 3) {
+    return numbers[str];
+  }
+
+  let result = '';
+
+  for (let i = length - 1, j = 0; i >= 0; i--, j++) {
+    // ten thousand
+    if (i === 4) {
+      if (str[j+1] === '0') result += tens[str[j]] + places[i];
+      else result += tens[str[j]];
+    }
+    else if (i === 1) {
+      result += tens[(str[j])];
+    } else {
+      result += numbers[str[j]] + places[i];
+    }
+  }
+
+  return result;
 }
+
+// console.log(numToWords(0));
+// console.log(numToWords(15));
+// console.log(numToWords(2999));
+// console.log(numToWords(40));
+// console.log(numToWords(31692));
 
 module.exports = numToWords;
