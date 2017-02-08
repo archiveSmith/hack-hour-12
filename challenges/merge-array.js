aa01dcfb0b1d0413354a1d5b36ee7d26ca78cae8
Brandon Smith <bxleesmith@gmail.com>
2017-02-08 09:13:33 -0800

    saving merge array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,28 +13,44 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
-  let i,j;
-  let mergedArr = [];
-  for (i = 0, j = 0; i < arr1.length && j < arr2.length;) {
-    if (arr1[i] <= arr2[j]) {
-      mergedArr.push(arr1[i]);
-      i += 1;
-    } else {
-      mergedArr.push(arr2[j]);
-      j += 1;
-    }
+// function mergeArrays(arr1, arr2) {
+//   let i,j;
+//   let mergedArr = [];
+//   for (i = 0, j = 0; i < arr1.length && j < arr2.length;) {
+//     if (arr1[i] <= arr2[j]) {
+//       mergedArr.push(arr1[i]);
+//       i += 1;
+//     } else {
+//       mergedArr.push(arr2[j]);
+//       j += 1;
+//     }
+//   }
+
+//   if (arr1.length === i) {
+//     mergedArr = mergedArr.concat(arr2.slice(j));
+//     return mergedArr;
+//   } else if (arr2.length === j) {
+//     mergedArr = mergedArr.concat(arr1.slice(i));
+//     return mergedArr;
+//   }
+
+//   return mergedArr;
+// }
+
+function mergeArrays(arr1, arr2, mergedArr = []) {
+  if (arr1.length === 0 || arr2.length === 0) {
+    let rest = arr1.length === 0 ? arr2 : arr1;
+    mergedArr = mergedArr.concat(rest);
+    return mergedArr;
   }
 
-  if (arr1.length === i) {
-    mergedArr = mergedArr.concat(arr2.slice(j));
-    return mergedArr;
-  } else if (arr2.length === j) {
-    mergedArr = mergedArr.concat(arr1.slice(i));
-    return mergedArr;
+  if (arr1[0] <= arr2[0]) {
+    let next = mergedArr.push(arr1[0]);
+    return mergeArrays(arr1.slice(1), arr2, next);
+  } else {
+    let next = mergedArr.push(arr2[0]);
+    
   }
-
-  return mergedArr;
 }
 
 // console.log(mergeArrays([1, 2, 4], [1, 7]))
