902659267e68fe42e73203e8b898f8d1acfe2f51
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-09 09:47:20 -0800

    permPalin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,15 @@
  */
 
 function permPalin(str) {
-	
+    str = str.split('');
+    i = 0;
+    while (str.length > i + 1) {
+      if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) i += 1;
+      if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) return false;
+      str.splice(str.lastIndexOf(str[i]), 1);
+	    str.shift();
+    }
+	  return true;
 }
 
 module.exports = permPalin;
