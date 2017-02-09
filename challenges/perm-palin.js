59f1aa161678845fcc994c50b46b6ccdbde24ce6
Curt Toppel <ctoppel@aol.com>
2017-02-09 09:58:27 -0800

    complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,35 @@
  */
 
 function permPalin(str) {
-	
+  const arr = Array.from(str);
+
+  const permutator = (inputArr) => {
+    const result = [];
+
+    const permute = (array, m = []) => {
+      if (array.length === 0) {
+        result.push(m);
+      } else {
+        for (let i = 0; i < array.length; i += 1) {
+          const curr = array.slice();
+          const next = curr.splice(i, 1);
+          permute(curr.slice(), m.concat(next));
+        }
+      }
+    };
+
+    permute(inputArr);
+
+    return result;
+  };
+  const perms = (permutator(arr));
+
+  for (let i = 0; i < perms.length; i += 1) {
+    if (perms[i].slice(0, ~~perms[i].length / 2).join('') ===
+      perms[i].slice(Math.ceil(perms[i].length / 2)).reverse().join(''))
+      return true;
+  }
+  return false;
 }
 
 module.exports = permPalin;
