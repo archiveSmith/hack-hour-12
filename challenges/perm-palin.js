045b5897341d6fed2cdd705b3dda04871f483cf8
hit1st <hit1st4731@sbcglobal.net>
2017-02-09 09:57:35 -0800

    perm-palin submit

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,27 @@
  */
 
 function permPalin(str) {
+	if (typeof str !== 'string') return 'str not a string';
+	if (str.length === 1) return true;
 	
+	const uniqueFrequency = {};
+	
+  for(var i = 0; i < str.length; i++){
+    uniqueFrequency[str[i]] = ++uniqueFrequency[str[i]]||1;
+  }
+
+  console.log(uniqueFrequency);
+  
+  let oddsCount = 0;
+  
+  for (let uniqueStr in uniqueFrequency) {
+    console.log(uniqueFrequency[uniqueStr]);  
+    if (uniqueFrequency[uniqueStr] % 2 !== 0) {
+      oddsCount += 1;
+    }
+    if (oddsCount > 1) return false;
+  }
+  return true;
 }
 
 module.exports = permPalin;
