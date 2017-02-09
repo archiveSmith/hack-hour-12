a6e476dd1c419980701c243ff455c4e56bd09410
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-09 09:54:55 -0800

    permPalin refactor

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,15 +10,16 @@
  */
 
 function permPalin(str) {
-    str = str.split('');
-    i = 0;
-    while (str.length > i + 1) {
-      if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) i += 1;
-      if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) return false;
-      str.splice(str.lastIndexOf(str[i]), 1);
-	    str.shift();
-    }
-	  return true;
+  str = str.split('');
+  i = 0;
+  while (str.length > 1) {
+    if (i === 1 && str.indexOf(str[i]) === str.lastIndexOf(str[i])) return false;
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) i = 1;
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) return false;
+    str.splice(str.lastIndexOf(str[i]), 1);
+    str.shift();
+  }
+  return true;
 }
 
 module.exports = permPalin;
