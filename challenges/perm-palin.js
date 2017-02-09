2483789cd41aa75dfe15d4dca39d0124e42ffa78
Admin <admin@Admins-MacBook-Pro.local>
2017-02-09 09:44:08 -0800

    finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,22 @@
  */
 
 function permPalin(str) {
-	
+    if (!str || typeof str !== 'string') return false;
+
+    const numOfEachChar = [];
+    let oddNumCount = 0;
+    while (str) {
+        let regex = new RegExp(str[0], 'ig');
+
+        numOfEachChar.push(str.match(regex).length);
+        str = str.replace(regex, '');
+    }
+    console.log(numOfEachChar);
+    return !(numOfEachChar.filter((num) => {
+        return (num % 2);
+    }).length > 1)
+
 }
+console.log(permPalin('abac'))
 
 module.exports = permPalin;
