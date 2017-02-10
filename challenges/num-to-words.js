1eb5f21e5e2d5de79d16514f1636638692c5eeef
Matthias Wagner <matzewagner@gmail.com>
2017-02-10 09:51:37 -0800

    second try

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -14,10 +14,11 @@
 
 function numToWords(num) {
   if (!num) return undefined;
+  if (num === 0) return 'Zero';
   
   const numStr = num.toString();
   const output = [];
-  const hash = { 0:'Zero',1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine',
+  const hash = { 0:'',1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine',
                 10:'Ten',11:'Eleven',12:'Twelve',13:'Thirteen',14:'Fourteen',15:'Fifteen',16:'Sixteen',17:'Seventeen',18:'Eighteen',19:'Nineteen',
                 20:'Twenty',30:'Thirty',40:'Forty',50:'Fifty',60:'Sixty',70:'Seventy',80:'Eighty',90:'Ninety',
               };
@@ -50,6 +51,6 @@ function numToWords(num) {
   return output.reverse().join('');
 }
 
-// console.log(numToWords(987654321));
+console.log(numToWords(987654021));
 
 module.exports = numToWords;
