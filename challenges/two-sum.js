3d70de46c0ffe613b9ae127784ffd53854fc071f
Matthias Wagner <matzewagner@gmail.com>
2017-02-13 09:24:05 -0800

    two-sum first commit

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,18 @@
  */
 
 function twoSum(arr, n) {
+  if (!arr || !n) return false;
 
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      // console.log('testing: ', arr[i], '+', arr[j], '===', n);
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
+  return false;
 }
+
+
+// console.log(twoSum([1,2,3,4,5], 9));
 
 module.exports = twoSum;
