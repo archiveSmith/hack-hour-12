145ccc250817d79b02c10ad119f1fcb82150a659
Joseph Vu <jvu009@ucr.edu>
2017-02-13 09:25:19 -0800

    finish two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,22 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+ 'use strict';
 
+function twoSum(arr, n) {
+  if (!Array.isArray(arr)) return;
+  if (arr.length === 0) return false;
+
+  const arrLen = arr.length;
+
+  for (let i = 0; i < arrLen - 1; i++) {
+    for (let j = i + 1; j < arrLen; j++) {
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
+
+  return false;
 }
 
 module.exports = twoSum;
