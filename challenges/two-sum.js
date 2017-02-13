e96f20a632759237c0a49a8f9f9dca2011a3402d
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-13 09:21:01 -0800

    Two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,19 @@
  */
 
 function twoSum(arr, n) {
-
+    for (let i = 0; i < arr.length; i++) {
+        const choosed = arr[i];
+        const rest = arr.slice(i + 1);
+        console.log(choosed, rest);
+        for (let j = 0; j < rest.length; j++) {
+            if (rest[j] + choosed === n) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 module.exports = twoSum;
+
+// console.log(twoSum([1,2,3,4,5], 9));
