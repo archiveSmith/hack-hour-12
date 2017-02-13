6476080e065d5fdd9df8ecbe43faf8e9f6137b8e
Deep Pulusani <sdeep27@yahoo.com>
2017-02-13 09:26:39 -0800

    finished twosum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
-
+  if(arguments.length < 2 || !arr[0]) return;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++){
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
+  return false;
 }
 
+//console.log(twoSum(19));
 module.exports = twoSum;
