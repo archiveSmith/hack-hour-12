ca252ac39a89b13f94d5f256831aab9d9ac6b84e
Joseph Vu <jvu009@ucr.edu>
2017-02-14 09:49:51 -0800

    finish valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,7 +3,7 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,7 +12,18 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+  function helperLeft(tree, n) {
+    if (tree === null) return true;
+    if (tree.value > n) return false;
+    return helperLeft(tree.left, tree.value) && helperRight(tree.right, tree.value);
+  }
+  function helperRight(tree, n) {
+    if (tree === null) return true;
+    if (tree.value <= n) return false;
+    return helperLeft(tree.left, tree.value) && helperRight(tree.right, tree.value);
+  }
 
+  return helperLeft(tree.left, tree.value) && helperRight(tree.right, tree.value);
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
