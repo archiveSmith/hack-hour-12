126a159a568e62ebd2f13fa8465d58052cf992f2
Richard Ta <richardtabusiness@gmail.com>
2017-02-15 10:06:31 -0800

    refactored

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -11,25 +11,31 @@
 // 8 = 1, 2, 4, 8
 
 function gcd(a, b) {
-    const divisor1 = [];
-    const divisor2 = [];
-    for(let i = a; i > 0; i--) {
-        if((a / i) % 1 === 0) {
-            divisor1.push(i);
-        }
-    }
-    for(let i = b; i > 0; i--) {
-        if((b / i) % 1 === 0) {
-            divisor2.push(i);
-        }
-    }
+    // const divisor1 = [];
+    // const divisor2 = [];
+    // for(let i = a; i > 0; i--) {
+    //     if((a / i) % 1 === 0) {
+    //         divisor1.push(i);
+    //     }
+    // }
+    // for(let i = b; i > 0; i--) {
+    //     if((b / i) % 1 === 0) {
+    //         divisor2.push(i);
+    //     }
+    // }
 
-    for(let i = 0; i < divisor1.length; i++) {
-        for(let j = 0; j < divisor2.length; j++) {
-            if(divisor1[i] === divisor2[j]) {
-                return divisor1[i];
-            }
-        }
+    // for(let i = 0; i < divisor1.length; i++) {
+    //     for(let j = 0; j < divisor2.length; j++) {
+    //         if(divisor1[i] === divisor2[j]) {
+    //             return divisor1[i];
+    //         }
+    //     }
+    // }
+
+    const obj = {a: [], b: []};
+    for(let i = ((a>b) ? a: b) ; i > 0; i--) {
+        if((a / i) % 1 === 0) obj['a'].push(i);
+        if((b / i) % 1 === 0 && obj['a'].includes(i)) return i;
     }
 }
 
