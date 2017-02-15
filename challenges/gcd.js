e78aa8711cdf673ae21b306fcdb75b4dff6f5554
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-15 09:32:01 -0800

    gcd fixed edge case

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,12 @@
  */
 
 function gcd(a, b) {
-
+    const y = a > b ? a : b;
+    const x = y === a ? b : a;
+    for (let i = Math.floor(y / 2); i > 0; i -= 1) {
+      if (y % i === 0 && x % i === 0 && x !== i) return i;
+    }
+    return 1;
 }
 
 module.exports = gcd;
