b32460e9bc7c29ba146e062daf51b858e8ff0ec1
skykeeper0 <trungdinh0429@gmail.com>
2017-02-15 09:39:49 -0800

    gcd challenge

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,39 @@
  *
  */
 
-function gcd(a, b) {
+function gcd(a = null, b = null) {
+    //edge case for bad input
+    if (!a || !b) return 'bad inputs'
 
+    // a === b the gcd is a or b
+    if (a == b) return a;
+
+    // the different maybe the greatest common divisor
+    for (let i = Math.abs(a - b); i > 0 ;i--) {
+        if (a % i === 0 && b % i === 0) {
+            return i
+        }
+    }
+    //
 }
+
+//============================ TESTING =====================================
+
+// let result1 = gcd(10, 8) 
+// console.log(result1);
+
+// let result2 = gcd(10, 9) 
+// console.log(result2)
+
+// let result3 = gcd(10, 5) 
+// console.log(result3)
+
+// let result4 = gcd(7, 7) 
+// console.log(result4)
+
+// let result5 = gcd(8) 
+// console.log(result5)
+
+//=================== DO NOT MODIFY BELOW ===============================
 
 module.exports = gcd;
