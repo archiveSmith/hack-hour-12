92d4b1bcf6b14e35e2c787b909442aef714a9008
Kyle Shaw <kyleshaw@Kyles-MacBook-Pro.local>
2017-02-15 09:55:30 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,18 @@
  */
 
 function gcd(a, b) {
+  const min = Math.min(a, b);
 
+  const gCD2 = (input) => {
+    if (a % input === 0 && b % input === 0) {
+      return Math.abs(input);
+    }
+    return gCD2(input - 1);
+  };
+  if (a === 0 || b === 0) {
+    return Math.max(Math.abs(a), Math.abs(b));
+  }
+  return gCD2(min);
 }
 
 module.exports = gcd;
