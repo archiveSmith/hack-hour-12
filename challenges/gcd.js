a3c628ad44df8f12bf98ac0f5afcd5d719c99d3a
Admin <admin@Admins-MacBook-Pro.local>
2017-02-15 09:25:19 -0800

    finished gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,14 @@
  */
 
 function gcd(a, b) {
+  if (!a || !b || typeof a !== 'number' || typeof b !== 'number') return 'Check Inputs';
 
+  let biggestDivisor = 1;
+  for (var i = 1; i <= (a > b ? a : b) / 2; i++) {
+    if (a % i === 0 && b % i === 0 && i > biggestDivisor) biggestDivisor = i;
+  } return biggestDivisor;
 }
+console.log(gcd(10, 9));
+console.log(gcd(10, 20));
 
 module.exports = gcd;
