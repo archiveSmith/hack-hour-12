165e763869e14a531f2d2a304616fce19bf6bc3d
hit1st <hit1st4731@sbcglobal.net>
2017-02-15 09:57:39 -0800

    submit0 gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,35 @@
  */
 
 function gcd(a, b) {
-
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return 'bad inputs';
+  
+  const divisorA = [];
+  const divisorB = [];
+  let shortList = [];
+  let longList = [];
+  let gcd = 1;
+  
+  for (let i = 1; i <= a; i += 1) {
+    if (a % i === 0) divisorA.push(i);
+  }
+  
+  for (let i = 1; i <= b; i += 1) {
+    if (b % i === 0) divisorB.push(i);
+  }
+  
+  if (divisorA.length <= divisorB.length) {
+    shortList = divisorA;
+    longList = divisorB;
+  } else {
+    shortList = divisorB;
+    longList = divisorA;
+  }
+  
+  for (let i = 0; i < shortList.length; i += 1) {
+    if (longList.includes(shortList[i]) && gcd <= shortList[i]) gcd = shortList[i];
+  }
+  
+  return gcd;
 }
 
 module.exports = gcd;
