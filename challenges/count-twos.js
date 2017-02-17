9e41014df580ee297f457ca8772c05745a095a26
Joseph Vu <jvu009@ucr.edu>
2017-02-17 09:51:41 -0800

    brute force

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,14 @@
 
 
 function countTwos(num) {
+  let count = 0;
+  for (let i = 0; i <= num; i += 1) {
+    count += i.toString().replace(/[^2]/g, '').length
+  }
 
+  return count;
 }
+
+console.log(countTwos(11420));
 
 module.exports = countTwos;
