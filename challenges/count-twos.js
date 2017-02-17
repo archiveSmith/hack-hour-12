4d69e731a239644a6ab77cc479c4e4a23557f3a8
codesmith <student@Mac-mini.local>
2017-02-17 09:52:39 -0800

    count-two

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -6,9 +6,24 @@
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
 
-
 function countTwos(num) {
+    let result = 0;
 
+    for (let i = 1; i <= num; i++) {
+        let str = i.toString();
+        result += (str.length - str.replace(/2/g,'').length);
+    }
+
+    console.log(result)
 }
+
+countTwos(1);  //-> 0
+countTwos(3);  //-> 1
+countTwos(13);  //-> 2
+countTwos(1000);  //-> 300
+countTwos(11420);  //-> 4483
+
+
+
 
 module.exports = countTwos;
