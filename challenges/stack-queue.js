8644a1821a8395f91076ec05b0d974656377b984
Joseph Vu <jvu009@ucr.edu>
2017-02-19 16:09:01 -0800

    refactor stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -16,7 +16,7 @@ Stack.prototype.push = function(value) {
 Stack.prototype.pop = function() {
   if (!this.length) return;
   const result = this.storage[--this.length];
-  delete this.storage[this.length]
+  delete this.storage[this.length];
   return result;
 };
 
@@ -27,25 +27,16 @@ function Queue() {
 }
 
 Queue.prototype.enqueue = function(value) {
-  if (this.stack2.length === 0) return this.stack2.push(value);
-
-  let curr = this.stack2.pop();
-  while (curr !== undefined) {
-    this.stack1.push(curr);
-    curr = this.stack2.pop();
-  }
-  this.stack2.push(value);
-
-  curr = this.stack1.pop();
-  while (curr !== undefined) {
-    this.stack2.push(curr);
-    curr = this.stack1.pop();
-  }
-
-  return this.stack2.length;
+  return this.stack1.push(value);
 };
 
 Queue.prototype.dequeue = function() {
+  if (this.stack2.length) return this.stack2.pop();
+
+  while (this.stack1.length) {
+    this.stack2.push(this.stack1.pop());
+  }
+  
   return this.stack2.pop();
 };
 
