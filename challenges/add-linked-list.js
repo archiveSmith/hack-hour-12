b916b7570235daee1323d68470efe30719dc57df
Schno Mozingo <schno.mozingo@gmail.com>
2017-02-27 10:12:34 -0800

    adding add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -17,8 +17,61 @@ function Node(val) {
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
+const l1 = new Node(1);
+l1.next = new Node(2);
+l1.next.next = new Node(3);
+const l2 = new Node(5);
+l2.next = new Node(6);
+l2.next.next = new Node(7);
+l2.next.next.next = new Node(8);
 
+
+function addLinkedList(l1, l2) {
+  // pick which linked list will be primary
+  let list1 = null;
+  let list2 = null;
+  let lastNode;
+  let sumList;
+
+  if( l1.value ) {
+    // make sure list 1 is valid
+    list1 = l1;
+    list2 = l2;
+  } else if (l2) {
+    // if it isn't, and list 2 is valid, just return it
+    return l2;
+  } else {
+    // it's all garbage, we're out
+    return;
+  }
+
+  while (list1 ||  list2) {
+    // get the value from a valid list
+    let value = list1 ? list1.value : 0;
+    value += list2 ? list2.value : 0;
+
+    // create the new node
+    node = new Node(value);
+
+    // if we're the first node, create the list
+    // otherwise, keep it going
+    if(lastNode) lastNode.next = node;
+    else sumList = node;
+
+    // walk the lists
+    lastNode = node;
+    list1 = list1 ? list1.next : null;
+    list2 = list2 ? list2.next : null;
+  }
+
+  return sumList;
+
+}
+
+let sl = addLinkedList(l1,l2);
+while(sl) {
+  console.log(sl.value);
+  sl = sl.next;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
