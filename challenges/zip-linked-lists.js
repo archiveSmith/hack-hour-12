2d9563cd0d401c4cd97402ec61b2a4723c281ac0
Mark Marcelo <markmarcelo@Marks-MBP.T-mobile.com>
2017-02-26 23:44:29 -0800

    zip-linked-list recursive

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,19 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+// Recursive Solution
+function zip (l1, l2) {
+  if (!l1) {
+    return l2;
+  }
+  if (!l2) {
+    return l1;
+  }
+  l1.next = zip(l2, l1.next);
+  return l1;
+}
+
+function zipAlternate(l1, l2) {
   const arr1 = [];
   const arr2 = [];
   let output;
