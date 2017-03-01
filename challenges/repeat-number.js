39f001e791b19898e31e84534c789957307d2588
Brandon Smith <bxleesmith@gmail.com>
2017-03-01 09:49:02 -0800

    checking repeat num

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -19,12 +19,12 @@
 // }
 
 function repeatNumbers(array) {
-  const expectedSum = (array.length * (array.length + 1)) / 2;
+  const expectedSum = (array.length * (array.length - 1)) / 2;
   let actualSum = array.reduce((acc, cv) => acc + cv);
   return actualSum - expectedSum;
 }
 
-const arr = [1, 2, 3, 4, 5, 7, 8, 5];
+const arr = [1, 2, 3, 4, 5, 5, 6, 7, 8];
 
 console.log(repeatNumbers(arr));
 
