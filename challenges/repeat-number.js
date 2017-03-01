9d2b8c50df04c9fae8e1ed7a67d150df56c97a78
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-01 09:18:16 -0800

    repeatNumbers

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,10 @@
  */
 
 function repeatNumbers(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    if (array.lastIndexOf(array[i]) > i) return array[i];
+  }
+  return 'Invalid input';
 }
 
 module.exports = repeatNumbers;
