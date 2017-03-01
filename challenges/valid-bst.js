d6d51987e2a27b10f1079ef070509f8e3849f881
Kyle Shaw <kyleshaw@Kyles-MacBook-Pro.local>
2017-03-01 09:52:32 -0800

    val-bst & rep-num

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -6,13 +6,23 @@
  
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
+  function makeArray(node) {
+    if (!node) return [];
+    return [...makeArray(node.left), node.value, ...makeArray(node.right)];
+  }
 
+  const entireArray = makeArray(tree);
+  for (let i = 1; i < entireArray.length; i += 1) {
+    if (entireArray[i] < entireArray[i - 1]) return false;
+  }
+
+  return true;
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
