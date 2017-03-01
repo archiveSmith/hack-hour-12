0fc4791dbbabb8234872ee2195c0323f4dcd5006
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-03-01 09:40:31 -0800

    got O(1) space, try for O(n) time

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,15 +11,26 @@
  */
 
 function repeatNumbers(array) {
-  let answer;
-  array.reduce(function(curr, next) {
-    if (curr.includes(next)) {
-      answer = next;
-    }
-    else curr.push(next);
-    return curr;
-  }, []);
-  return answer;
+
+    if (!array) return null;
+
+    let i;
+    let len = array.length;
+    for (i = 0; i < len; i += 1) {
+      let indNum = array.shift();
+      if (array.includes(indNum)) return indNum;
+    }   
+  }
+
+  // let answer;
+  // array.reduce(function(curr, next) {
+  //   if (curr.includes(next)) {
+  //     answer = next;
+  //   }
+  //   else curr.push(next);
+  //   return curr;
+  // }, []);
+  // return answer;
 }
 
 module.exports = repeatNumbers;
