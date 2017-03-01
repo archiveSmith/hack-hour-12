ecc40cb1e838ffdff55d1e067ce87f66f6154d4f
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-01 09:20:19 -0800

    done with repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,15 @@
  *
  */
 
+const array = [];
+
 function repeatNumbers(array) {
 
+  for (let i = 0, k = 1; i < array.length; i++, k++) {
+    if(array[i] === array[k]) return array[i];
+  }
 }
+
+console.log(repeatNumbers(array));
 
 module.exports = repeatNumbers;
