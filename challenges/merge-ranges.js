0610ee0dd64832b28b4b33729563746bbf9e375e
Deep Pulusani <sdeep27@yahoo.com>
2017-03-02 10:26:01 -0800

    finished merged ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -8,10 +8,17 @@
  *
  * Do not assume the ranges are in order
  */
-
+'use strict';
 
 function mergeRanges(array) {
-
+  array.sort((a,b) => a[0]-b[0])
+  for (let i = 0; i < array.length-1; i++){
+    if (array[i+1][0]-array[i][1] < 1) {
+      array[i][1] = array[i+1][1];
+      array.splice(i+1,1);
+    }
+  }
+  return array;
 }
-
+//console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]));
 module.exports = mergeRanges;
