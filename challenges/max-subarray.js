c4465c35535e535c221e4393cd6ff77121877a32
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-04 09:29:27 -0800

    maxSubarray with alternate solution

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -22,4 +22,16 @@ function maxSubarray(arr) {
   return max;
 }
 
+function maxSubarrayOptimal(arr) {
+
+  var currentMax = Number.NEGATIVE_INFINITY;
+  var finalMax = Number.NEGATIVE_INFINITY;
+
+  for (var i = 0; i < arr.length; i++) {
+    currentMax = Math.max(arr[i], currentMax + arr[i]);
+    finalMax = Math.max(finalMax, currentMax);
+  }
+  return finalMax;
+}
+
 module.exports = maxSubarray;
