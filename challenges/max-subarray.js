c8e7f049056d89637c1c5f52479c1f450f5d8e11
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-04 09:31:31 -0800

    adding max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -6,9 +6,21 @@
  *              maxSubarray([15,20,-5,10])  -> 40
  *
  */
-
+//maxSubarray([15,20,-5,10]) // 40
+//maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]) // 18
 function maxSubarray(arr) {
-
+  let sum = 0;
+  // loop through
+  for (let i = 0, k = 1; k < arr.length; i++, k++) {
+      // console.log(i +': ' + arr[i] , k + ': ' + arr[k]);
+    if(arr[i] < arr[k]) {
+      sum += arr[i] + arr[k];
+      // console.log('i',i,'k',k,'sum',sum);
+    } else if (sum === 0) {
+      sum += arr[0];
+    }
+  }
+  return sum;
 }
 
 module.exports = maxSubarray;
