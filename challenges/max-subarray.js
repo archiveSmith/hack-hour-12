e02c2aeef0feb93d83946693d5c3f852c8d681d0
Brandon Smith <bxleesmith@gmail.com>
2017-03-05 22:14:58 -0800

    checking max sub

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,20 +7,34 @@
  *
  */
 
-function maxSubarray(arr) {
-  console.log(arr);
-  if (arr.length === 1) return arr[0];
-  let arrSum = arr.reduce((acc, cv) => acc + cv);
-  console.log('sum', arrSum);
-  let compare = Math.max(maxSubarray(arr.slice(1)), maxSubarray(arr.slice(0, -1), maxSubarray(1, -1)));
-  console.log(compare)
-  return Math.max(arrSum, compare);
-}
+// function maxSubarray(arr) {
+//   console.log(arr);
+//   if (arr.length === 1) return arr[0];
+//   let arrSum = arr.reduce((acc, cv) => acc + cv);
+//   console.log('sum', arrSum);
+//   let compare = Math.max(maxSubarray(arr.slice(1)), maxSubarray(arr.slice(0, -1), maxSubarray(1, -1)));
+//   console.log(compare)
+//   return Math.max(arrSum, compare);
+// }
 
 function maxSubarray(arr) {
-  
+  let max = arr.reduce((acc, cv) => acc + cv);
+  // console.log('max', max)
+  for (let i = 0; i < arr.length; i += 1) {
+    let currSum;
+    // console.log(max)
+    for (let j = i + 1; j < arr.length; j += 1) {
+      let subSum = arr[i] + arr[j];
+      if (subSum > max) max = subSum;
+      currSum += subSum;
+    }
+
+    if (currSum > max) max = currSum;
+  }
+
+  return max;
 }
 
-console.log(maxSubarray([2, -5]))
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]))
 
 module.exports = maxSubarray;
