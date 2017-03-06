ecdffea79114ba15e4ff81ee32763f8485638b0a
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-06 09:28:45 -0800

    updated with more efficient resursive solution

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -43,4 +43,19 @@ function hasCycle(head) {
   return false;
 }
 
+function hasCycleOptimal(linkedList) {
+  if (linkedList && linkedList.next) {
+    return findCycle(linkedList, linkedList.next);
+  }
+  return false;
+}
+
+function findCycle(tortoise, hare) {
+  if (tortoise === hare) {
+    return true;
+  }
+  return hare !== null && hare.next !== null && findCycle(tortoise.next, hare.next.next);
+}
+
+
 module.exports = {Node: Node, hasCycle: hasCycle}
