620efb6034f14e10f367fb536b1c4907681b1b8a
skykeeper0 <student@Mac-mini.local>
2017-03-06 09:12:29 -0800

    multiple of 3 or 5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -6,11 +6,41 @@
 // below 1000 and return that sum.
 
 function sumMultiples3Or5Below1000() {
-  let sum = 0;
+  let LCM = function(x,y) {
+    let  i = 1;
 
-  return sum;
+    while (!(i % x === 0 && i % y === 0)) {
+      i++;
+    }
+
+    return i;
+  }
+
+  let sumSingle = function(x,roof) {
+    for (let i = roof; i >= 0; i--) {
+      if (i % x === 0) {
+        let n = i / x;
+        return x * (n+1) * n / 2 ;
+      } 
+    }
+  }
+
+  let LCMvalue = LCM(3,5);
+
+  return sumSingle(3,10) + sumSingle(5,10) - sumSingle(LCMvalue,10);
 }
 
+let sumSingle = function(x,roof) {
+  for (let i = roof; i >= 0; i--) {
+    if (i % x === 0) {
+      let n = i / x;
+      return x * (n+1) * n / 2 ;
+    } 
+  }
+}
+
+console.log(sumMultiples3Or5Below1000());
+console.log(sumSingle(5,10))
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
