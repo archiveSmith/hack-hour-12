409759bf55ff98a88e30753c870db0d53917a641
Brandon Smith <bxleesmith@gmail.com>
2017-03-06 09:29:27 -0800

    checking cycle linked list

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -12,27 +12,29 @@
  *
  * Create a function that accepts a linked list and returns true if the linked list has a cyclical reference
  *
- * var node1 = new Node('1');
- * var node2 = node1.next = new Node('2');
- * var node3 = node2.next = new Node('3');
- * var node4 = node3.next = new Node('4');
- * var node5 = node4.next = new Node('5');
- * hasCycle(node1); // => false
- * node5.next = node2;
- * hasCycle(node1); // => true
  *
  * Challenge 1: Do this in linear time
  * Challenge 2: Do this in constant space
  * Challenge 3: Do not mutate the original nodes in any way
  *
  */
-
 var Node = function(value) {
   this.value = value;
   this.next = null;
 }
 
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// // console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
+
+
 function hasCycle(head) {
+  if (head) return false;
   const nodes = [];
   let cycle = false;
   let currNode = head;
