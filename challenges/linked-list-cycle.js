f3ae8947def369b587872f73c09a06bd6456c257
Matthias Wagner <matzewagner@gmail.com>
2017-03-06 09:34:00 -0800

    modified edge cases

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,25 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
+  if (!head || !head.next) return false;
+  let n1 = head;
+  let n2 = head.next.next;
 
+  while (n1 !== n2) {
+    if (!n2.next || !n2.next.next) return false;
+    n2 = n2.next.next;
+    n1 = n1.next;
+  }
+  return true;
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
