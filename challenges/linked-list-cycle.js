0978c9fe9ddb6586ab7bd900c827d13ff28bb337
Brandon Smith <bxleesmith@gmail.com>
2017-03-06 09:47:25 -0800

    fixing errors cycle linked list

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -23,25 +23,25 @@ var Node = function(value) {
   this.next = null;
 }
 
-// var node1 = new Node('1');
-// var node2 = node1.next = new Node('2');
-// var node3 = node2.next = new Node('3');
-// var node4 = node3.next = new Node('4');
-// var node5 = node4.next = new Node('5');
+var node1 = new Node('1');
+var node2 = node1.next = new Node('2');
+var node3 = node2.next = new Node('3');
+var node4 = node3.next = new Node('4');
+var node5 = node4.next = new Node('5');
 // console.log(hasCycle(node1)); // => false
-// node5.next = node2;
-// console.log(hasCycle(node1)); // => true
+node5.next = node2;
+console.log(hasCycle(node1)); // => true
 
 
 function hasCycle(head) {
   if (!head) return false;
-  const nodes = {};
+  const nodes = [];
   let cycle = false;
   let currNode = head;
   while (!cycle) {
-    if (nodes[JSON.stringify(currNode)]) return true;
+    if (nodes.includes(currNode)) return true;
     if (currNode.next === null) return false;
-    nodes[JSON.stringify(currNode)] = true;
+    nodes.push(currNode);
     currNode = currNode.next;
   }
 }
