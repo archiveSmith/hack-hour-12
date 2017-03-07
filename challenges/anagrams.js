747cd9e929c28ffd155d3d9fb08139f2707e3ff1
Matthias Wagner <matzewagner@gmail.com>
2017-03-07 09:51:51 -0800

    anagram take 1b

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,24 @@
   */
 
 function anagrams(string) {
-
+  if (!string) return;
+  const output = [];
+  const used = [];
+  function recurse(str) {
+    const chars = str.split('');
+    for (let i = 0; i < chars.length; i += 1) {
+      const curr = chars.splice(i, 1);
+      used.push(curr);
+      if (chars.length === 0) output.push(used.join(''));
+      recurse(chars.join(''));
+      chars.splice(i, 0, curr);
+      used.pop();
+    }
+  }
+  recurse(string);
+  return output;
 }
+
+// console.log(anagrams('abc'));
 
 module.exports = anagrams;
