9b3d18745e7aeee4a4f7163def24e037c0bff1a1
Curt Toppel <ctoppel@aol.com>
2017-03-07 09:39:18 -0800

    complete

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,19 @@
   */
 
 function anagrams(string) {
-
+  if (string.length < 2) {
+    return [string];
+  }
+  const allAnswers = [];
+  for (let i = 0; i < string.length; i += 1) {
+    const letter = string[i];
+    const shorterWord = string.substr(0, i) + string.substr(i + 1, string.length - 1);
+    const shortstringArray = anagrams(shorterWord);
+    for (let j = 0; j < shortstringArray.length; j += 1) {
+      allAnswers.push(letter + shortstringArray[j]);
+    }
+  }
+  return allAnswers;
 }
 
 module.exports = anagrams;
