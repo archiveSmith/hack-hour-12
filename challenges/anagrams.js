030971076b75dac5f659f3709531dcd8f56d2874
Deep Pulusani <sdeep27@yahoo.com>
2017-03-07 09:35:45 -0800

    finished anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,34 @@
   */
 
 function anagrams(string) {
-
+  function swap (str, spot1, spot2) {
+    let arr = str.split('');
+    let temp = arr[spot1];
+    arr[spot1] = arr[spot2];
+    arr[spot2] = temp;
+    return arr.join('');
+  }
+  function swapAll (string) {
+    const result = [string];
+    for (let i = 0; i < string.length; i++) {
+      for (let j = 0; j < string.length; j++) {
+        if(i !== j) result.push(swap(string, i, j));
+      }
+    }
+    return result;
+  }
+  const holderArr = swapAll(string);
+  const allCombos = [];
+  for (let i = 0; i < holderArr.length; i++) {
+    allCombos.push(swapAll(holderArr[i]));
+  }
+  const slimCombos = allCombos.reduce((result, item) => {
+    return result.concat(item);
+  }, [])
+  return slimCombos.filter((item, index) => {
+    return slimCombos.indexOf(item) === index;
+  })
 }
+//console.log(anagrams('dare'));
 
 module.exports = anagrams;
