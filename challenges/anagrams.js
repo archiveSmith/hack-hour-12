1956486b3b3a2f7a814ffad545cea5efda5b3213
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-07 09:23:10 -0800

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,21 @@
   */
 
 function anagrams(string) {
+  const results = [];
+  let stringArr = string.split('');
 
+  function perm(items, cur = []) {
+    if (cur.length === string.length) return results.push(cur);
+    let c;
+    for (let i = 0; i < items.length; i += 1) {
+      c = items.splice(i, 1)[0];
+      perm(items.slice(), cur.concat(c));
+      items.splice(i, 0, c);
+    }
+  }
+
+  perm(stringArr);
+  return results;
 }
 
 module.exports = anagrams;
