3869b3f85b8af0976941ae5b394d11db5bd08f72
skykeeper0 <student@Mac-mini.local>
2017-03-08 09:16:30 -0800

    anagram

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,34 @@
   */
 
 function anagrams(string) {
+  //sepertate string into array
+  let arr = string.split(''); // ['a','b','c']
+  console.log(arr);
+  // create permutation
+  let result = arr;
 
+
+  for (let i = 0; i < arr.length - 1; i++) {
+      let new_result = [];
+      result.forEach((ele) => {
+        arr.forEach( (ele2) => {
+          let str = ele + ele2;
+          if (str.uniq().length == str.length) {
+            new_result.push(str)
+          }
+
+        })
+      result = new_result
+    })
+
+
+    console.log(result)
+  }
+
+  return result;
 }
+
+var result = anagrams('abc');
+console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
 
 module.exports = anagrams;
