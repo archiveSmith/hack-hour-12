a5458bf584e6aa5a34d4d64151ea105771c398e9
Curt Toppel <ctoppel@aol.com>
2017-03-08 09:40:02 -0800

    complete

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,31 +10,38 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
- const Node = (value) => {
-   this.value = value;
-   this.next = null;
- };
+ let Node = function(value) {
+  this.value = value;
+  this.next = null;
+}
 
- function deleteDups(head) {
-   const compArr = [];
-   let newHead = {};
-   let currNode = head;
-
-   while (currNode) {
-     compArr.push(currNode.value);
-     currNode = currNode.next;
-   }
-
-   const noDupsArr = Array.from([...new Set(compArr)]);
-   newHead = new Node(noDupsArr[0]);
-   currNode = newHead;
-
-   for (let i = 1; i < noDupsArr.length; i += 1) {
-     currNode.next = new Node(noDupsArr[i]);
-     currNode = currNode.next;
-   }
-
-   return newHead;
- }
+function deleteDups(head) {
+  const compArr = [];
+  let newHead = {};
+  let currNode = head;
+  while (currNode) {
+    compArr.push(currNode.value);
+    currNode = currNode.next;
+  }
+  // console.log(compArr)
+  // console.log(currNode)
+  // currNode = head;
+  // console.log(currNode)
+  // while (currNode && currNode.next) {
+  //   if (compArr.includes(currNode.next.value)) {
+  //     currNode.next = currNode.next.next;
+  //   }
+  //   currNode = currNode.next;
+  // }
+  let noDupsArr = Array.from([...new Set(compArr)])
+  console.log(noDupsArr)
+  newHead = new Node(noDupsArr[0]);
+  currNode = newHead;
+  for (let i = 1; i < noDupsArr.length; i += 1) {
+    currNode.next = new Node(noDupsArr[i]);
+    currNode = currNode.next;
+  }
+  return newHead;
+}
 
  module.exports = deleteDups;
