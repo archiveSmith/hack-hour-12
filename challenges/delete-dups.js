99bfd0a7332abad99a0c3112c7bf1d702a3fc53e
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-08 09:33:50 -0800

    adding anagrams and delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,46 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
-
-function deleteDups(head) {
-
+function Node (value) {
+  this.value = value;
+  this.next = null;
 }
+
+const n1 = new Node('1');
+n1.next = new Node('2');
+n1.next.next = new Node('3');
+n1.next.next.next = new Node('4');
+n1.next.next.next.next = new Node('3');
+n1.next.next.next.next.next  = new Node('4');
+n1.next.next.next.next.next.next  = new Node('5');
+n1.next.next.next.next.next.next.next = new Node('6');
+
+function deleteDups(node) {
+  const values = [node.value];
+
+  while (node.next) {
+    while(values.includes(node.next.value)) {
+      const deleteMe = node.next;
+      node.next = node.next.next;
+      delete deleteMe;
+    }
+
+    values.push(node.next.value);
+
+    node = node.next;
+  }
+}
+
+function walk (node) {
+  while(node) {
+    console.log('Val', node.value);
+    node = node.next;
+  }
+  console.log('======')
+}
+
+//walk(n1);
+//deleteDups(n1);
+//walk(n1);
 
 module.exports = deleteDups;
