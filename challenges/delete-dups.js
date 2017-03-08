ff20b5ae0193bbfa3c616a832b8fbfecc52f888e
Joseph Vu <jvu009@ucr.edu>
2017-03-08 09:28:55 -0800

    fix bug

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,10 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-function Node(value) {
-  this.value = value;
-  this.next = null;
-}
+// function Node(value) {
+//   this.value = value;
+//   this.next = null;
+// }
 
 function deleteDups(head) {
   const list = {};
@@ -34,14 +34,14 @@ function deleteDups(head) {
   return head;
 }
 // 1 -> 2 -> 3 -> 3 -> 3 -> 4 -> 5 -> null
-const node = new Node(1);
-node.next = new Node(2);
-node.next.next = new Node(3);
-node.next.next.next = new Node(3);
-node.next.next.next.next = new Node(3);
-node.next.next.next.next.next = new Node(4);
-node.next.next.next.next.next.next = new Node(5);
-console.log(JSON.stringify(node));
-console.log(JSON.stringify(deleteDups(node)));
+// const node = new Node(1);
+// node.next = new Node(2);
+// node.next.next = new Node(3);
+// node.next.next.next = new Node(3);
+// node.next.next.next.next = new Node(3);
+// node.next.next.next.next.next = new Node(4);
+// node.next.next.next.next.next.next = new Node(5);
+// console.log(JSON.stringify(node));
+// console.log(JSON.stringify(deleteDups(node)));
 
 module.exports = deleteDups;
