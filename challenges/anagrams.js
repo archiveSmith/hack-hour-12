ce4c03f2995d10f208a806a3c394561d0221a3fe
shahrodkh <shahrodkh@gmail.com>
2017-03-08 10:18:47 -0800

    anagrams solved

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -11,12 +11,37 @@
   * var result = anagrams('abc');
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
-
+'use strict';
 function anagrams(string) {
   // Store results in object for constant lookup time
+  const permutations = {};
+
   // Create driver function for getting perms
-    // Base case: If we have used all elements from str AND we haven't stored this anagram already, save
+  function getPerms(str, cur = '') {
+    // Base case: If we have used all elements from str, save anagram as key. Value does not matter.
+    if (str === '') return permutations[cur] = true;
+
+    // Loop through string
+    for (let i = 0; i < str.length; i += 1) {
+      // Take letter at current index
+      const selected = str.slice(i, i + 1);
+
+      // Store the rest of the letters for the next call
+      const rest = str.slice(0, i) + str.slice(i + 1);
+
+      // Pass the leftover letters and the built up current string to a recursive call
+      getPerms(rest, cur + selected);
+    }
+    // Return null to end recursive calls declaritively
+    return null;
+  }
+
+  // Call getPerms on string input
+  getPerms(string);
+  console.log(Object.keys(permutations));
 
 }
 
 module.exports = anagrams;
+
+anagrams('abc');
