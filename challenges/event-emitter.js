83ca9fa4705f89b9626c9250c016c4c03aa24958
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-09 09:25:40 -0800

    event-emitter without first caveats

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,26 @@
  */
 
 function EventEmitter() {
-
+  this.events = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  this.events[funcName] = func;
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  this.events[funcName](...args);
 };
+
+var instance = new EventEmitter();
+var counter = 0;
+instance.on('increment', function() {
+  counter++;
+}); // counter should be 0
+console.log(counter);
+instance.trigger('increment'); // counter should be 1
+console.log(counter);
+instance.trigger('increment'); // counter should be 2
+console.log(counter);
 
 module.exports = EventEmitter;
