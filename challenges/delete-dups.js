1aae08183b982b9a435eff6051b80c143af01836
Admin <admin@Admins-MacBook-Pro.local>
2017-03-09 09:22:41 -0800

    finished event-emitter?

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -16,28 +16,39 @@ function Node(val) {
 }
 
 function deleteDups(head) {
-  if (!head) return false;
-  const found = [];
-  let currNode = head;
+  // if (!head) return false;
+  // const found = [];
+  // let currNode = head;
 
-  found.push(currNode.value);
+  // found.push(currNode.value);
 
-  while (currNode.next) { console.log(found);
-    if (found.indexOf(currNode.next.value) >= 0) {
-      currNode.next = currNode.next.next;
-      continue;
-    } 
-    found.push(currNode.next.value);
-    currNode = currNode.next;
+  // while (currNode.next) { console.log(found);
+  //   if (found.indexOf(currNode.next.value) >= 0) {
+  //     currNode.next = currNode.next.next;
+  //     continue;
+  //   } 
+  //   found.push(currNode.next.value);
+  //   currNode = currNode.next;
+  // }
+  // return head;
+
+  function recurse(currNode, laterNode, nodeBeforeLaterNode) {
+    if (!laterNode) return;
+    if (currNode.value === laterNode.value) {
+      nodeBeforeLaterNode.next = nodeBeforeLaterNode.next.next; 
+    }
+    recurse(currNode, laterNode.next, nodeBeforeLaterNode.next);
+    recurse(currNode.next, laterNode.next, nodeBeforeLaterNode.next);
   }
+  recurse(head, head.next, head);
   return head;
 }
 
-// let head = new Node(1);
-// node1 = head.next = new Node(2);
-// node2 = node1.next = new Node(3);
-// node3 = node2.next = new Node(3);
-// console.log(JSON.stringify(head));
-// console.log(JSON.stringify(deleteDups(head)));
+let head = new Node(1);
+node1 = head.next = new Node(2);
+node2 = node1.next = new Node(3);
+node3 = node2.next = new Node(3);
+console.log(JSON.stringify(head));
+console.log(JSON.stringify(deleteDups(head)));
 
 module.exports = deleteDups;
