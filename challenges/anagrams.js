c19729b10513ea2fac794b7933bf89dee7be6d2c
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-09 09:08:35 -0800

    Delete dups

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,23 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+// Permutations are factorials functions.
 
+function anagrams(string) {
+  const permutations = {};
+  function getPerms(str, cur = '') {
+    if (str === '') return permutations[cur] = true;
+    for (let i = 0; i < str.length; i++) {
+      const selected = str.charAt(i);
+      const rest = str.slice(0, i) + str.slice(i + 1);
+      getPerms(rest, cur + selected);
+    }
+    return null;
+  }
+  getPerms(string);
+  return Object.keys(permutations);
 }
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -11,9 +11,46 @@
  */
 
 
-
 function deleteDups(head) {
+  const store = {};
+  const clone = JSON.parse(JSON.stringify(head));
+  clone.value = null;
+  clone.next = null;
+  let currentNode = head;
+  while (currentNode) {
+    if (!store[currentNode.value]) {
+      store[currentNode.value] = true;
+    }
+    currentNode = currentNode.next;
+  }
 
+  let cloneCurrentNode = clone;
+  let headClone = cloneCurrentNode;
+  for (let prop in store) {
+    while (cloneCurrentNode.next !== null) {
+      cloneCurrentNode = cloneCurrentNode.next;
+    }
+    console.log(prop);
+    cloneCurrentNode.value = prop;
+  }
+  console.log('headClone', JSON.stringify(headClone));
 }
+
+var Node = function(value) {
+  this.value = value;
+  this.next = null;
+}
+
+var node1 = new Node('1');
+var node2 = node1.next = new Node('2');
+var node3 = node2.next = new Node('3');
+var node4 = node3.next = new Node('4');
+var node5 = node4.next = new Node('5');
+var node6 = node5.next = new Node('3');
+// console.log(node1);
+
+console.log(JSON.stringify(deleteDups(node1)));
+
+
 
 module.exports = deleteDups;
