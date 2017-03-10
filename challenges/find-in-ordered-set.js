510ea6eebf1f89544bd59b71e187d16b5ab81080
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-10 09:36:09 -0800

    adding find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,8 +9,24 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
+var nums = [1, 4, 6, 7, 9, 17, 45,47]
+//console.log(findInOrderedSet(nums,47)); // -> true
 
 function findInOrderedSet(arr, target) {
+  // get the midpoint of the array
+  let half = Math.floor(arr.length / 2);
+  // base case - we found nothing
+  if (half === 0) return false;
+
+  let firstHalf = arr.slice(0,half);
+  let secondHalf = arr.slice(half);
+
+  // return if it's a match
+  if (target === arr[half]) return true;
+  else if (target < arr[half]) return findInOrderedSet(firstHalf,target);
+  else return findInOrderedSet(secondHalf, target);
+
+  return false;
 
 }
 
