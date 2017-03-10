773b03aa79707d05250686b66da93b7d6b1227dc
Joseph Vu <jvu009@ucr.edu>
2017-03-10 09:34:32 -0800

    check edge cases

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,9 +11,11 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  if (!Array.isArray(arr)) return false;
   if (arr.length === 0) return false;
   const mid = ~~(arr.length / 2);
   if (arr[mid] === target) return true;
+  if (arr[mid] > arr[arr.length - 1]) return false;
   return arr[mid] > target ? findInOrderedSet(arr.slice(0, mid), target) : findInOrderedSet(arr.slice(mid), target);
 }
 
