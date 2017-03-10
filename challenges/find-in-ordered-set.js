a9511b311090ab1adcb1c51309258fbd7a1542eb
Richard Ta <richardtabusiness@gmail.com>
2017-03-10 09:20:57 -0800

    updated brute force solution

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,10 +11,12 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-  arr.forEach((num) => {
-    if(num === target) return;
-  })
-}
 
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === target) return true;
+  }
+  return false;
+
+}
 
 module.exports = findInOrderedSet;
