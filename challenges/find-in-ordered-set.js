361418f735b6346f8e91ebccda7b1c349fc057ef
skykeeper0 <trungdinh0429@gmail.com>
2017-03-10 09:48:10 -0800

    find in order set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,29 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  let start = 0
+  let end = arr.length - 1;
+  
+  while (start + 1 != end) {
+    let total = start + end;
+    let mid = (total - total % 2) / 2;
 
+    if (arr[mid] == target) {
+      return true;
+    }
+
+    if (target > arr[mid]){
+      start = mid;
+    } else if ( target < arr[mid]) {
+      end = mid
+    } 
+  }
+
+  return false;
 }
 
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// result = findInOrderedSet(nums, 10);
+// console.log(result)
 
 module.exports = findInOrderedSet;
