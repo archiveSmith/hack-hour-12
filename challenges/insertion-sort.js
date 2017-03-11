a96b5aa4e3c958cdddac6fbec4f89c1e8241a154
Joseph Vu <jvu009@ucr.edu>
2017-03-11 09:28:04 -0800

    finish insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,27 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    const curr = array[i];
+    let inserted = false;
 
+    for (let j = i - 1; j >= 0; j--) {
+      if (curr < array[j]) {
+        array[j + 1] = array[j];
+      } else {
+        array[j + 1] = curr;
+        inserted = true;
+        break;
+      }
+    }
+
+    if (!inserted) {
+      array[0] = curr;
+    }
+  }
+
+  return array;
 }
 
 module.exports = insertionSort;
