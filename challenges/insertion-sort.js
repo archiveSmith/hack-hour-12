fe2c2c5600ac88264142a04468aa9343a980ca94
skykeeper0 <trungdinh0429@gmail.com>
2017-03-11 09:20:10 -0800

    insertionsort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,24 @@
 
 
 
-function insertionSort(array) {
+function insertionSort(arr) {
+    let i = 0;
 
+    while (i < arr.length) {
+
+       if (arr[i] > arr[i + 1]) {
+            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+            i = i - 1;
+       } else {
+            i = i + 1;
+       }
+    }
+
+    return arr;    
 }
 
+//================ TEST ===================
+// array = [3, 7 ,4 ,9, 5, 2, 6 ,1]
+// result = insertionSort(array)
+// console.log(result)
 module.exports = insertionSort;
