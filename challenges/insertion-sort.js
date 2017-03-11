9b2b1ca9a3bc22ddec2d762d9faf677247a561a3
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-03-11 09:37:10 -0800

    brain not working, poor first attempt

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
-
+  if (!array) return [];
+  let finalArr = [];
+  let i;
+  for (i = 0; i < array.length; i += 1) {
+    let counter = 1;
+    finalArr.push(array[i]);
+    while (finalArr[i] - counter !== undefined && finalArr[i] < finalArr[i - counter]) {
+      finalArr[i - counter + 1] = finalArr[i - counter];
+      counter += 1;
+    }
+    finalArr[i - counter] = array[i];
+  }
+  return finalArr;
 }
 
 module.exports = insertionSort;
