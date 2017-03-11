a110664a9f68500827e00d29f62a87616605f6ea
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-11 09:34:10 -0800

    inserted those things

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    let sortC = array.splice(i, 1)[0];
+    for (let j = i - 1; j >= -1; j -= 1) {
+      if (sortC >= array[j] || j === -1) {
+        array.splice(j + 1, 0, sortC);
+        break;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
