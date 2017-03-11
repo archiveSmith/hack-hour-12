92007c9a24c0c1907163ea2eb3d5f27a22b6fe76
Matthias Wagner <matzewagner@gmail.com>
2017-03-11 09:06:05 -0800

    refactoring

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -12,14 +12,13 @@ findInOrderedSet(nums, 2);  -> false
 
 function findInOrderedSet(arr, target) {
   if (!arr || !target) return;
-  if (target < arr[0]) return false;
-  if (arr.length <= 1 && arr[0] !== target) return false;
+  if (target < arr[0] || (arr.length <= 1 && arr[0] !== target)) return false;
   const split = Math.floor(arr.length / 2);
   if (target === arr[split]) return true;
   return target < arr[split] ? findInOrderedSet(arr.slice(0, split), target) : findInOrderedSet(arr.slice(split), target);
 }
 
-// var nums = [1, 4, 6, 7, 9, 17, 45]
+// var nums = [2, 4, 6, 7, 9, 17, 45]
 // console.log(findInOrderedSet(nums, 1));
 
 module.exports = findInOrderedSet;
