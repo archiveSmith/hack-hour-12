78480a3f3faea171c92ef85640b4b3396dbedf71
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-11 09:21:39 -0800

    Insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    let j = i;
+    while (array[j] > array[j+1]) {
+      const tmp = array[j];
+      array[j] = array[j+1];
+      array[j+1] = tmp;
+      j -= 1;
+    }
+  }
+  return array;
 }
+
+console.log(insertionSort([3,6,4,5,1,2,7,8]));
+
 
 module.exports = insertionSort;
