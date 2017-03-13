6a6fa45fa8d1b707c7f8402c1ae4ff2bdfe873cb
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-03-12 17:21:12 -0700

    one try w insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,19 +5,29 @@
 
 
 function insertionSort(array) {
+  function swap(a, b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
   if (!array) return [];
-  let finalArr = [];
+  // Check each element against each that came before it
   let i;
   for (i = 0; i < array.length; i += 1) {
-    let counter = 1;
-    finalArr.push(array[i]);
-    while (finalArr[i] - counter !== undefined && finalArr[i] < finalArr[i - counter]) {
-      finalArr[i - counter + 1] = finalArr[i - counter];
-      counter += 1;
+    let prevChecked = array.slice(0, i);
+    let temp = array[i];
+    let notSwapped = true;
+    let numToCheck = i - 1;
+    while (notSwapped && numToCheck > 0) {
+      if (temp > array[numToCheck]) notSwapped = true;
+      else {
+        swap(array[numToCheck], array[numToCheck + 1]);
+      }
+      numToCheck -= 1;
     }
-    finalArr[i - counter] = array[i];
   }
-  return finalArr;
+  return array;
 }
 
 module.exports = insertionSort;
