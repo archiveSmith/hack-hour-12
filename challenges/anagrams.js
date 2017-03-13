49eeae7655d3a436c33dfca44b355061a4d0c975
Richard Ta <richardtabusiness@gmail.com>
2017-03-12 22:30:18 -0700

    updated anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,28 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+// take combo for first letter, and then move first letter to end of string (rotate it) and then repeat this... string.length times
 function anagrams(string) {
+  const permutations = {};
+  function getPerms(str, curr = '') {
+    if(str === '') return permutations[curr] = true;  
 
+    for(let i = 0; i < str.length; i++) {
+
+      const selected = str.slice(i, i + 1);
+
+      const rest = str.slice(0, i) + str.slice(i + 1);
+
+      getPerms(rest, cur + selected);
+
+    }
+
+    return null;
+
+  }
+
+  getPerms(string);
+  return Object.keys(permutations);
 }
 
 module.exports = anagrams;
