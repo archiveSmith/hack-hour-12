21996c8feb764aa89310868bd82f7e39ff71a985
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-13 09:39:20 -0700

    adding unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -4,13 +4,31 @@
  *
  * uniqueNumber([1,2,1,3,3]); -> 2
  *
+ * [8,9,8,9,3]
+ *
  * BONUS:
  * Complete the challenge in O(n) time
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
 
+//console.log(uniqueNumber([8,9,8,9,3]));// -> 2
+function uniqueNumber(array) {
+  const res = {};
+
+  for (let i = 0; i < array.length; i++) {
+    res[array[i]] = 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    res[array[i]]++;
+  }
+
+  for (let key in res) {
+    if(res[key] === 1) {
+      return key;
+    }
+  }
 }
 
 module.exports = uniqueNumber;
