f10081fc513bc86de530afcaecd45519407f85f1
Matthias Wagner <matzewagner@gmail.com>
2017-03-14 09:29:15 -0700

    get-all-products take 1

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,15 @@
  */
 
 function getAllProducts(array) {
-
+  if (!array || array.length <= 1) return;
+  const output = [];
+  let current;
+  for (let i = 0; i < array.length; i += 1) {
+    current = array.slice(0, i).concat(array.slice(i + 1, array.length));
+    output.push(current.reduce((acc, val) => acc * val));
+  }
+  return output;
 }
 
+// console.log(getAllProducts([1,7,3,4]));
 module.exports = getAllProducts;
--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -19,8 +19,16 @@ function uniqueNumber(array) {
   }
   let result;
   return Object.keys(store)[0];
-  
 }
+
+// Bitwise XOR
+// function uniqueNumber(array) {
+//   let result = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+//     result = result ^ array[i];
+//   }
+//   return result;
+// }
 
 // console.log(uniqueNumber([1,2,1,3,3,2,4,5,4]));
 
