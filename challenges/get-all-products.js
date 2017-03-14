6bdfa8cb96c79c85f5808dbc7c0f9adc9ed8efd1
Joseph Vu <jvu009@ucr.edu>
2017-03-14 09:16:35 -0700

    finish get all products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,21 @@
  */
 
 function getAllProducts(array) {
+  if (!Array.isArray(array)) return;
+  const products = [];
 
+  for (let i = 0; i < array.length; i++) {
+    let product = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j) {
+        product *= array[j];
+      }
+    }
+
+    products.push(product);
+  }
+
+  return products;
 }
 
 module.exports = getAllProducts;
