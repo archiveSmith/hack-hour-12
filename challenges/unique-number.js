acf372e40df7a52c949b453c7b08cb69a8accb4c
Brandon Smith <bxleesmith@gmail.com>
2017-03-14 09:06:00 -0700

    saving unique num

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,24 +9,37 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
-  let actualSum = 0;
-  let mySet = new Set();
+// function uniqueNumber(array) {
+//   if (!array.length) return;
+//   if (array.length === 1) return array[0];
 
-  for (const num of array) {
-    actualSum += num;
-    mySet.add(num);
+//   let actualSum = 0;
+//   let mySet = new Set();
+
+//   for (const num of array) {
+//     actualSum += num;
+//     mySet.add(num);
+//   }
+
+//   let expectedSum = 0;
+
+//   for (const num of mySet) {
+//     expectedSum += num;
+//   }
+
+//   return (2 * expectedSum) - actualSum;
+// }
+
+function uniqueNumber(array, i = 0, num = array[i]) {
+  if (i === array.length) return;
+  for (let j = i + 1; j < array.length; j += 1) {
+    console.log(array[j], num)
+    if (array[j] === num) return uniqueNumber(array, i += 1); 
   }
 
-  let expectedSum = 0;
-
-  for (const num of mySet) {
-    expectedSum += num;
-  }
-
-  return (2 * expectedSum) - actualSum;
+  return num;
 }
 
-// console.log(uniqueNumber([1,2,1,3,2,3,4]));
+console.log(uniqueNumber([1,2,1,3,2,3,4]));
 
 module.exports = uniqueNumber;
