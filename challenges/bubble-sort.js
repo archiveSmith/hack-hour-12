6589a4f69368b6511818bace4cb7944f94de75ea
Kyle Shaw <kyleshaw@Kyles-MacBook-Pro.local>
2017-03-17 09:46:50 -0700

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let inSequence = false;
+  while (!inSequence) {
+    inSequence = true;
+    for (let i = 0; i < array.length - 1; i += 1) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        inSequence = false;
+      }
+    }
+  }
 }
+
 
 module.exports = bubbleSort;
