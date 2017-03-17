0513a40351cfb9a6e786e25e223c81e89bd8c7de
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-17 09:13:03 -0700

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,17 @@
 
 
 function bubbleSort(array) {
-
+  for (let j = 0; j < array.length; j += 1) {
+    for (let i = 0; i < array.length - 1; i += 1) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]]; 
+      }
+    }
+  }
+  return array;
 }
+
+// var a = [5,4,1,6,7,4,8];
+// console.log(bubbleSort(a));
 
 module.exports = bubbleSort;
