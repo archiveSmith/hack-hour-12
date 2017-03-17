4b29181beb6685966456658efd07bd9d04ac21e2
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-03-17 09:18:37 -0700

    bubble sort is working

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,31 @@
 
 
 function bubbleSort(array) {
+  function swap(arr, ind1, ind2) {
+    let temp = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = temp;
+  }
 
+  let i = array.length - 1;
+  let j = 0;
+  let changed = true;
+
+  while (changed && i >= 0) {
+    let j = 0;
+    let changed = false;
+    while (j < i) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+        changed = true;
+      }
+      j += 1;
+    }
+    i -= 1;
+  }
+  return array;
 }
+
+console.log(bubbleSort([6, 2, 4, 9, 1, 5]));
 
 module.exports = bubbleSort;
