85b58eb20a93d757f03d5562bd96b58db1f2eeb8
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-17 09:17:49 -0700

    Bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,23 @@
 
 
 function bubbleSort(array) {
-
+  let swap = false;
+  console.log(array)
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i+1]) {
+      const tmp = array[i];
+      array[i] = array[i+1];
+      array[i+1] = tmp;
+      swap = true;
+    }
+  }
+  if (swap === true) {
+    bubbleSort(array);
+  } else {
+    return array;
+  }
 }
+
+// console.log(bubbleSort([6,1,4,3,2,5,9,7,8]))
 
 module.exports = bubbleSort;
