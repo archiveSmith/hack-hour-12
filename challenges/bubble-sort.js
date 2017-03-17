17078d0442612a5586178089b6e32a7cd38a2b18
Curt Toppel <ctoppel@aol.com>
2017-03-17 09:15:35 -0700

    complete

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,14 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
-function bubbleSort(array) {
-
+function bubbleSort(array, n = array.length) {
+  if (n === 0) return array;
+  for (let i = 0; i < n; i += 1) {
+    if (array[i + 1] < array[i]) {
+      array[i] = array[i + 1];
+    }
+  }
+  return bubbleSort(array, n - 1);
 }
 
 module.exports = bubbleSort;
