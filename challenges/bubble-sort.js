1735157bba891a8982d0b03e9f87cdffce8e6506
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-20 09:53:13 -0700

    Bubble

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,23 +5,26 @@
 
 
 function bubbleSort(array) {
-  let swap = false;
-  console.log(array)
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > array[i+1]) {
-      const tmp = array[i];
-      array[i] = array[i+1];
-      array[i+1] = tmp;
-      swap = true;
+
+  function recurse(array, n) {
+    let swap = false;
+    for (let i = 0; i < n; i++) {
+      if (array[i] > array[i+1]) {
+        const tmp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = tmp;
+        swap = true;
+      }
+    }
+    if (swap === true) {
+      return recurse(array, n - 1);
+    } else {
+      return array;
     }
   }
-  if (swap === true) {
-    bubbleSort(array);
-  } else {
-    return array;
-  }
+  return recurse(array, array.length);
 }
 
-// console.log(bubbleSort([6,1,4,3,2,5,9,7,8]))
+console.log(bubbleSort([6,1,4,3,2,5,9,7,8]))
 
 module.exports = bubbleSort;
