cd3b13afafe4d75a0fc0aaf04c9137c63f82d7ce
Kyle Shaw <kyleshaw@Kyles-MacBook-Pro.local>
2017-03-20 09:32:33 -0700

    count-stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,17 @@
  */
 
 function countStairs(n) {
+  const arr = [];
+  arr[0] = 0;
+  arr[1] = 1;
+  arr[2] = 2;
 
+  for (let i = 3; i <= n; i += 1) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
 }
+
+console.log(countStairs(5));
 
 module.exports = countStairs;
