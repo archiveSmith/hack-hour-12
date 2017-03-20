e6b2390798bcbc3b303a5b55c53b4c77dcab46ae
shahrodkh <shahrodkh@gmail.com>
2017-03-20 09:50:25 -0700

    wee

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -26,12 +26,29 @@
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  jazbook.forEach(el => {
+    if (el[0] === name) return true;
+    return false;
+  });
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
+  const phoneBook = {};
 
+  jazbook.forEach(el => {
+    phoneBook[el[0]] = el[1];
+  });
+
+  phoneBook.add = function (name, phoneNumber) {
+    phoneBook[name] = phoneNumber;
+  };
+
+  phoneBook.remove = function (name) {
+    delete phoneBook[name];
+  };
+
+  return phoneBook;
 }
 
 const objectToExport = {
@@ -40,3 +57,24 @@ const objectToExport = {
 };
 
 module.exports = objectToExport;
+
+
+// const jazbook = [
+//   ['alex', '301-844-3421'],
+//   ['jae', '301-844-1211'],
+//   ['david', '301-844-0978'],
+//   ['travis', '301-844-8505'],
+//   ['jasmine', '1800-974-4539'],
+// ];
+
+// const phoneBook = makePhoneBookObject(jazbook);
+
+// console.log(phoneBook);
+
+
+// phoneBook.add('shahrod', '310-404-9832');
+
+// phoneBook.remove('david');
+
+
+// console.log(phoneBook);
