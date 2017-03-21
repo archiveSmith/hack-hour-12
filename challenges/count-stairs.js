d336ac4576fac601ddd1f9e62e7ab1d4ebc609a5
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-21 09:39:38 -0700

    add count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,8 +14,12 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
-function countStairs(n) {
+// Returns the count of ways we can sum  S[0...m-1] coins to get sum n
 
+function countStairs(n) {
+  if (n < 2) return 1;
+  return countStairs(n-2) + countStairs(n-1);
 }
+console.log(countStairs(5));
 
 module.exports = countStairs;
