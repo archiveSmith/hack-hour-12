bf68c6b80f89bff7e0228a33efe34bfcd57bb542
Admin <admin@Admins-MacBook-Pro.local>
2017-03-24 09:24:56 -0700

    finished observed-pin

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -43,7 +43,31 @@ expectations = {
 
 
 function getPINs(observed) {
+  const possibleNums = {
+    0: [0, 8],
+    1: [1, 2, 4],
+    2: [1, 2, 3, 5],
+    3: [2, 3, 6],
+    4: [1, 4, 5, 7],
+    5: [2, 4, 5, 6, 8],
+    6: [3, 5, 6, 9],
+    7: [4, 7, 8],
+    8: [5, 7, 8, 9, 0],
+    9: [6, 8, 9]
+  };
+  const variations = [];
 
+  function getVariations(observed, variation = '') {
+    if (observed === '') {
+      variations.push(variation);
+      return;
+    }
+    for (var i = 0; i < possibleNums[observed[0]].length; i++) {
+      getVariations(observed.slice(1), variation + possibleNums[observed[0]][i]);
+    }
+  }
+  getVariations(observed);
+  return variations;
 }
 
 
