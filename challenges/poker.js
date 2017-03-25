fe66f0469199ecad704a71701db022efef3ac787
Curt Toppel <ctoppel@aol.com>
2017-03-25 10:17:52 -0700

    complete

--- challenges/poker.js
+++ challenges/poker.js
@@ -18,8 +18,77 @@
 * Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
 */
 
-function poker(hand1, hand2) {
+// function createHand() {
+//   const hand = [];
+//
+//   for (let i = 0; i < 5; i += 1) {
+//     hand.push((~~(Math.random() * 13) + 2));
+//   }
+//
+//   return hand;
+// }
+//
+// const hand1 = createHand();
+// const hand2 = createHand();
 
+function poker(hand1, hand2) {
+  console.log(hand1);
+  console.log(hand2);
+
+  function findHandStrength(array) {
+    const compObj = {};
+
+    array.forEach((card) => {
+      if (compObj.hasOwnProperty(card)) {
+        // console.log('true')
+        compObj[card] += 1;
+      }
+      else compObj[card] = 1;
+    });
+
+    console.log(compObj);
+    // high card 2-14
+    // pair card val * 10
+    // 3-of-a-kind val * 100
+    // straight val * 1000
+    // full house val * 10000
+    // 4-of-a-kind val * 100000
+    // const compArr = Object.keys(compObj).sort((a, b) => a - b);
+    // console.log(compArr)
+    let result = 0;
+    for (let key in compObj) {
+      if (compObj[key] === 4) {
+        result = key * 1000000;
+      }
+      if (compObj[key] === 3) {
+        result = key * 100;
+      }
+      if (compObj[key] === 2) {
+        if (key * 10 > result) result = key * 10;
+      }
+      if (compObj[key] === 1) {
+        if (key > result) result = key;
+      }
+    }
+    if (result > 140) {
+      console.log('FULL HOUSE!!!')
+      for (let key in compObj) {
+        if (compObj[key] === 2) result *= 1000
+      }
+    }
+    if (result === 0) {
+      console.log('STRAIGHT!!!')
+      const compArr = Object.keys(compObj).sort((a, b) => a - b);
+      console.log(compArr)
+      if (compArr[4] - compArr[0] === 4) result = compArr[4] * 1000;
+    }
+
+    console.log(result)
+    return result;
+  }
+
+  // console.log(findHandStrength(hand2))
+  return findHandStrength(hand1) > findHandStrength(hand2) ? "Player 1 wins" : "Player 2 wins";
 }
 
 module.exports = poker;
