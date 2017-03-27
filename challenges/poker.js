32fc856c4b7f627984d1c80dde83b9367f5e9652
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-27 13:49:33 -0700

    poker refactor

--- challenges/poker.js
+++ challenges/poker.js
@@ -45,23 +45,37 @@ function poker(hand1, hand2) {
 
   function getWeight(handObj) {
     let weight = 0;
-    const highCard = [];
+    const highCard = [0];
 
     for (let card in handObj) {
+
+      // 4-of-a-kind
       if (handObj[card] === 4) {
-        weight += 600 + Number(card);
+        weight += 600;
       }
+
+      // 3-of-a-kind
       if (handObj[card] === 3) {
-        weight += 300 + Number(card);
+        weight += 300;
       }
+
+      // pair(s)
       if (handObj[card] === 2) {
-        weight += 100 + Number(card);
+        weight += 100;
       }
+
+      // prepare high card
       if (handObj[card] === 1) {
         highCard.push(Number(card));
       }
     }
-    return weight + Number(Math.max(...highCard));
+
+    // straight
+    highCard.sort((a, b) => b - a);
+    if (highCard[0] - highCard[4] === 4) weight += 350;
+
+    // add high card
+    return weight + highCard[0];
   }
 
   const player1 = getWeight(hand1token);
@@ -71,6 +85,16 @@ function poker(hand1, hand2) {
   return player1 > player2 ? 'Player 1 wins' : 'Player 2 wins';
 }
 
-console.log(poker([3,5,5,5,2], [4,6,7,8,8]));
+// // player 1 wins
+// console.log(poker([3,5,5,5,2], [4,6,7,8,8]));
+// console.log(poker([3,3,5,5,11], [2,2,3,3,8]));
+// console.log(poker([4,4,4,8,8], [2,3,4,5,6]));
+// console.log(poker([3,11,4,2,5], [4,6,7,8,10]));
+
+// // player 2 wins
+// console.log(poker([4,4,8,8,8], [3,5,5,5,5]));
+// console.log(poker([2,2,3,3,8], [3,3,5,5,11]));
+// console.log(poker([4,4,4,8,9], [2,3,4,5,6]));
+// console.log(poker([4,6,7,7,7], [3,4,5,6,7]));
 
 module.exports = poker;
