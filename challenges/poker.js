d4892d7c1b1baaa4ae2bc0a10f9620a5a05a19f2
skykeeper0 <trungdinh0429@gmail.com>
2017-03-26 23:56:35 -0700

    poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -20,6 +20,61 @@
 
 function poker(hand1, hand2) {
 
+  function cardRank(arr) {
+    // define a variable that let us see if it a 4 kind(3) or just 1 pair(1)
+    let cardTrackingObj = {};
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (!cardTrackingObj.hasOwnProperty(arr[i])) {
+        cardTrackingObj[arr[i]] = 1;
+      } else {
+        cardTrackingObj[arr[i]] += 1;
+      }
+    }
+
+    // check if the arr is straight or no
+    let straight = null;
+    if ((Math.max(arr) - Math.min(arr)) === 4) {
+      straight = true;
+    }
+
+    // 3 value type point and highcard point ( equal to the value of the highest repeated car or )
+    let typePoint = 0;
+    let highestValueAndRepeatedCard;
+
+    const cardNumber = Object.keys(cardTrackingObj).length;
+    const highestAppearingCount = Object.keys(cardTrackingObj).reduce( (highestCount, card) => {
+      if (highestCount <= Number(cardTrackingObj[card])) {
+        if (!highestValueAndRepeatedCard || highestValueAndRepeatedCard < card) {
+          highestValueAndRepeatedCard = Number(card);
+        }
+        return Number(cardTrackingObj[card]);
+      }
+      return highestCount;
+    }, 1)
+
+    if (highestAppearingCount === 2) typePoint = 300;
+    if (highestAppearingCount === 2 && cardNumber === 4) typePoint = 200;
+    if (highestAppearingCount === 3) typePoint = 400;
+    if (straight) typePoint = 500;
+    if (highestAppearingCount === 3 && cardNumber === 2) typePoint = 600;
+    if (highestAppearingCount === 4) typePoint = 700;
+
+    // console.log(typePoint)
+
+    return typePoint + highestValueAndRepeatedCard;
+  }
+
+  if (cardRank(hand1) >  cardRank(hand2)) {
+    return "Player 1 wins";
+  } else if (cardRank(hand2) >  cardRank(hand1)) {
+    return "Player 2 wins";
+  } else {
+    return "Draw";
+  }
+  // return cardRank(hand1);
 }
 
+// let result  = poker([8,5,6,9,7], [4,6,8,8,8]);
+// console.log(result);
 module.exports = poker;
