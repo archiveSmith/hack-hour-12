0c89d89c046ceded0fce9154dc0939dce7b9c74f
Kyle Shaw <kyleshaw@Kyles-MBP.attlocal.net>
2017-03-26 20:59:43 -0700

    observed pin

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -43,7 +43,29 @@ expectations = {
 
 
 function getPINs(observed) {
+  // Object mapping out possibilities based on observed digit.
+  const adj = {
+    0: ['8', '0'],
+    1: ['1', '2', '4'],
+    2: ['1', '2', '3', '5'],
+    3: ['2', '3', '6'],
+    4: ['1', '4', '5', '7'],
+    5: ['2', '4', '5', '6', '8'],
+    6: ['3', '5', '6', '9'],
+    7: ['4', '7', '8'],
+    8: ['5', '7', '8', '9', '0'],
+    9: ['6', '8', '9'],
+  };
 
+  // If there is only one observed digit, return array of possibilities from the object above.
+  if (observed.length === 1) return adj[observed]
+
+  // Get the remaining possibilities from remaining digits.
+  const theRest = getPINs(observed.slice(1))
+
+  // Place each possible digit from current observed digit and place it front of every possibility from
+  // above array of possibilities (based on remaining observed digits). Return big array of possibilities.
+  return adj[observed[0]].reduce((accum, num) => [...accum, ...theRest.map(pins => num.concat(pins))], []);
 }
 
 
