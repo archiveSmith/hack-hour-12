b3d6fbd700c80f299a0894423e117f62318658fc
Curt Toppel <ctoppel@aol.com>
2017-03-28 16:45:29 -0700

    complete

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,10 +14,10 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
- function countStairs(n , memo = {}) {
-   if (memo.hasOwnProperty(n)) return memo[n];
-   if (n < 2) return 1;
-   return memo[n] = countStairs(n - 1, memo) + countStairs(n - 2, memo);
- }
+function countStairs(n, memo = { 0: 0, 1: 1 }) {
+  return memo.hasOwnProperty(n) ?
+    memo[n] :
+    memo[n] = countStairs(n - 1, memo) + countStairs(n - 2, memo);
+}
 
 module.exports = countStairs;
