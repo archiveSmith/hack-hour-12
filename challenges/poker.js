be1390dd095990bf67a255921aec8f212ef41307
Curt Toppel <ctoppel@aol.com>
2017-03-28 09:04:12 -0700

    complete

--- challenges/poker.js
+++ challenges/poker.js
@@ -1,22 +1,22 @@
-/*Build a function that takes in two arrays of five elements, with each
-* array representing a player's hand and each element
-* representing a card from a standard deck of cards:
-* 52 cards, 2 - Ace, NO suits. (Jack = 11, Queen = 12, King = 13, Ace = 14)
-*
-* The function should return either the string "Player 1 wins", "Player 2 wins", or "Draw"
-*  based on the two player's hands:
-*
-* 4-of-a-kind > full house > straight > 3-of-a-kind > 2-pair > 1-pair > high card.
-* (keeping in mind how cards out-rank eachother and possible draws)
-*
-* Start by hard-coding the array/hand inputs, but if there is time also
-* create a deck of cards function that creates the two arrays for you,
-* keeping in mind that there are only 4 of each card in a deck (52 total).
-*
-* BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
-*
-* Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
-*/
+ /* Build a function that takes in two arrays of five elements, with each
+  * array representing a player's hand and each element
+  * representing a card from a standard deck of cards:
+  * 52 cards, 2 - Ace, NO suits. (Jack = 11, Queen = 12, King = 13, Ace = 14)
+  *
+  * The function should return either the string "Player 1 wins", "Player 2 wins", or "Draw"
+  *  based on the two player's hands:
+  *
+  * 4-of-a-kind > full house > straight > 3-of-a-kind > 2-pair > 1-pair > high card.
+  * (keeping in mind how cards out-rank eachother and possible draws)
+  *
+  * Start by hard-coding the array/hand inputs, but if there is time also
+  * create a deck of cards function that creates the two arrays for you,
+  * keeping in mind that there are only 4 of each card in a deck (52 total).
+  *
+  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
+  *
+  * Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
+  */
 
 // function createHand() {
 //   const hand = [];
@@ -36,14 +36,12 @@ function poker(hand1, hand2) {
   // console.log(hand2);
 
   function findHandStrength(array) {
+    array = array.sort((a, b) => a - b);
+
     const compObj = {};
 
     array.forEach((card) => {
-      if (compObj.hasOwnProperty(card)) {
-        // console.log('true')
-        compObj[card] += 1;
-      }
-      else compObj[card] = 1;
+      compObj.hasOwnProperty(card) ? compObj[card] += 1 : compObj[card] = 1;
     });
 
     console.log(compObj);
@@ -56,6 +54,8 @@ function poker(hand1, hand2) {
     // const compArr = Object.keys(compObj).sort((a, b) => a - b);
     // console.log(compArr)
     let result = 0;
+
+    // compObj.
     for (let key in compObj) {
       if (compObj[key] === 4) {
         result = key * 1000000;
