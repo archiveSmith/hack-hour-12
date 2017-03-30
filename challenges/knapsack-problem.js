8cc8c5d7ae3af9e45d2bd885facff8c19f9b6964
Curt Toppel <ctoppel@aol.com>
2017-03-30 09:36:01 -0700

    complete

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,10 +10,11 @@
 */
 
 function solveKnapsack(items, weightAvailable, value = 0) {
+  console.log(items, weightAvailable);
   if (weightAvailable === 0) return value;
   if (weightAvailable < 0) return 9;
   return solveKnapsack(items.slice(1), weightAvailable -= items[0].weight, value += items[0].value) ||
-  solveKnapsack(items.slice(1), weightAvailable, value)
+  solveKnapsack(items.slice(1), weightAvailable, value);
 }
 
 module.exports = solveKnapsack;
