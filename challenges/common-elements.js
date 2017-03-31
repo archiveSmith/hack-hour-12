104171f5b9761302b19f40e7fdd59cbdad41dff7
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-31 09:38:57 -0700

    adding common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,37 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
+  const items = {};
+  const matches = [];
 
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < arguments[i].length; j++) {
+      if(items[arguments[i][j]]){
+        items[arguments[i][j]][i] = 1;      }
+      else {
+        items[arguments[i][j]] = {};
+        items[arguments[i][j]][i] = 1;
+      }
+    }
+  }
+
+  for (let item in items) {
+    if (Object.keys(items[item]).length === 4) matches.push(item);
+  }
+  console.log(matches);
+  return matches;
 }
+
+console.log(commonElements(array1,array2,array3,array4));
 
 module.exports = commonElements;
