d9bd476bbfec1a0ee240a10fa85e4c13537d9a3c
Joseph Vu <jvu009@ucr.edu>
2017-03-31 09:20:41 -0700

    finish common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,35 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+
+	console.log(commonElements(array1, array2, array3, array4));
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
+	// validate input
+	if (!Array.isArray(array1) || !Array.isArray(array2) || !Array.isArray(array3) || !Array.isArray(array4)) return;
 
+	const common = [array1, array2, array3, array4].reduce((acc, next) => {
+		const newArray = [];
+
+		next.forEach((ch) => {
+			if (acc.includes(ch) && !newArray.includes(ch)) newArray.push(ch);
+		});
+
+		return newArray;
+	});
+
+	// Nothing in common
+	if (common.length === 0) return 'Nothing in Common!';
+
+	return common;
 }
 
 module.exports = commonElements;
