9e735f1b406cccd5af9ceb27c74784046c2f0709
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-04-02 20:37:35 -0700

    first try parse dates

--- challenges/parse-dates.js
+++ challenges/parse-dates.js
@@ -38,9 +38,79 @@
 // - you can assume the provided day of month will be valid if it's a month string
 //   (i.e. the function will not be called with 'Jul 84th 1:00 PM') since that's not a real date
 // - if any part of the date string is missing then you can consider it an invalid date
+const DAYS_OF_WEEK = {
+  Sunday: 0,
+  Monday: 1,
+  Tuesday: 2,
+  Wednesday: 3,
+  Thursday: 4,
+  Friday: 5,
+  Saturday: 6
+};
+
+const MONTHS = {
+  Jan: 0,
+  Feb: 1,
+  Mar: 2,
+  Apr: 3,
+  May: 4,
+  Jun: 5,
+  Jul: 6,
+  Aug: 7,
+  Sep: 8,
+  Oct: 9,
+  Nov: 10,
+  Dec: 11
+};
 
 function parseDates(str) {
-  console.log(str);
+  const date = new Date();
+
+  if (!/^[a-zA-Z]{3,9}\s(\d{1,2}[a-z]{2}\s)?\d{1,2}:\d{2}\s(AM|PM)$/.test(str)) return date;
+
+  const sections = str.split(' ');
+  let month, dayOfMonth, times, hour, minutes, ampm;
+  if (sections[0] === 'Today') {
+    times = sections[1].split(':');
+    ampm = sections[2];
+  }
+  if (MONTHS.hasOwnProperty(sections[0])) {
+    month = MONTHS[sections[0]];
+    dayOfMonth = Number(sections[1].match(/\d+/));
+    times = sections[2].split(':');
+    ampm = sections[3];
+    // Mutate Date object with input's month and day
+    date.setMonth(month);
+    date.setDate(dayOfMonth);
+  }
+  if (DAYS_OF_WEEK.hasOwnProperty(sections[0])) {
+    day = sections[0];
+    times = sections[1].split(':');
+    ampm = sections[2];
+    // Move one day back in case day of week matches today
+    date.setDate(date.getDate() - 1);
+    // Loop backwards one day at a time checking if day of week matches
+    while (date.getDay() !== DAYS_OF_WEEK[day]) {
+      // Note that this works even if month changes!
+      date.setDate(date.getDate() - 1);
+    }
+  }
+
+  hour = Number(times[0]);
+  minutes = Number(times[1]);
+
+  // Midnight should be 0 instead of 12
+  if (ampm === 'AM' && hour === 12) hour = 0;
+  // Non-noon PM hours should increase by 12. Think military time.
+  if (ampm === 'PM' && hour !== 12) hour += 12;
+  
+  // Mutate Date object with input's hour and minutes
+  date.setHours(hour);
+  date.setMinutes(minutes);
+  return date;
 }
+
+
+
 
 module.exports = parseDates;
