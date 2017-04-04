e7be46cf725b500878850136814c60c569cab638
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-04-03 23:17:29 -0700

    removed console logs and unnecessary comments

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,13 +13,10 @@
 
 
 function deleteDups(head) {
-  console.log(head);
   let valueArr = [];
-  // check that we are not at end of LL
   while (head.next !== null) {
     valueArr.push(head.value);
     let temp = null;
-    // Reassign next pointer as long as next.value is a duplicate
     if (valueArr.includes(head.next.value)) {
       temp = head.next;
       while (valueArr.includes(temp.value)) {
@@ -36,16 +33,5 @@ function Node (value) {
   this.value = value;
   this.next = null;
 }
-
-const n1 = new Node(1);
-n1.next = new Node(2);
-n1.next.next = new Node(3);
-n1.next.next.next = new Node(3);
-n1.next.next.next.next = new Node(3);
-n1.next.next.next.next.next = new Node(5);
-n1.next.next.next.next.next.next = new Node(4);
-n1.next.next.next.next.next.next.next = new Node(2);
-
-
 
 module.exports = deleteDups;
