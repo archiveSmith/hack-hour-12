4f26d90f193503b7a915f5ace613f2c4bfe06d86
skykeeper0 <trungdinh0429@gmail.com>
2017-04-04 22:53:16 -0700

    added each permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -20,10 +20,38 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
+function eachPermutation(arr, cb) {
+  //set up mem storage for permutation
+  const mem = [];
 
+  function createPermutation(arr, result = []) {
+    //base case
+    if (arr.length === 0 && mem.indexOf(result) === -1) {
+      mem.push(result);
+      return;
+    }
+
+    //iterate using for loop
+    for (let i = 0; i < arr.length; i++) {
+      const new_result = result.concat(arr[i]);
+      const new_arr = arr.slice(0,i).concat(arr.slice(i + 1))
+      createPermutation(new_arr, new_result)
+    }
+
+    return;
+  }
+
+  createPermutation(arr)
+
+  mem.forEach( (ele) => {
+    cb(ele)
+  })
 }
 
+// testing
+// const result = eachPermutation([1,2,3], function(ele) {
+//   console.log(ele);
+// });
 
 
 module.exports = eachPermutation;
