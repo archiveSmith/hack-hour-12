81a43d53691acb2831dd4f912b36ca8de9bcd752
Trung Dinh <skykeeper0@skykeeper0.local>
2017-04-11 18:20:14 -0700

    working on roman-numberal

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,15 @@
  */
 
 function romanNumeral(n) {
-    
+    const numberTable = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
+    }
 }
 
 module.exports = romanNumeral;
--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -19,17 +19,17 @@ function subsetSum(array, target) {
     }
 }
 
-var arr1 = [3,7,4,2]
-console.log(subsetSum(arr1, 5)) // true
-var arr2 = [3,34,4,12,5,12]
-console.log(subsetSum(arr2, 32))  
-var arr3 = [8,2,4,12]
-console.log(subsetSum(arr3, 13)) // false 
-var arr4 = [8,-2,1,-3]
-console.log(subsetSum(arr4))
-var arr5 = [6]
-console.log(subsetSum(arr5, 6))
-var arr6 = []
-console.log(subsetSum(arr6, 6))
+// var arr1 = [3,7,4,2]
+// console.log(subsetSum(arr1, 5)) // true
+// var arr2 = [3,34,4,12,5,12]
+// console.log(subsetSum(arr2, 32))  
+// var arr3 = [8,2,4,12]
+// console.log(subsetSum(arr3, 13)) // false 
+// var arr4 = [8,-2,1,-3]
+// console.log(subsetSum(arr4))
+// var arr5 = [6]
+// console.log(subsetSum(arr5, 6))
+// var arr6 = []
+// console.log(subsetSum(arr6, 6))
 module.exports = subsetSum;
 
