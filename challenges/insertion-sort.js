48f22df4e2aff870b141f382709bc95426dc73bd
Richard Ta <richardta@ucla.edu>
2017-05-08 23:20:43 -0700

    add comments

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,24 +2,25 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
+  // Make copy of original array so we don't alter the original array
   let result = array.slice();
+  // initialize j variable to hold the current index when finding place to insert
   let j;
   let toInsert;
   for(let i = 1; i < result.length; i++) {
     toInsert = result[i];
     j = i;
+    // loop until you find the index to insert
     while(j >= 0 && toInsert < result[j - 1]) {
+      // move elements to the right to find the index to insert
       result[j] = result[j-1];
       j--;
     }
+    // insert the value into correct index at j
     result[j] = toInsert;
   }
   return result;
 }
-
-// console.log(insertionSort([1,5,3,2,66,44,33,75,23]));
 
 module.exports = insertionSort;
