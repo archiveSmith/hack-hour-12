edabe76ffeb95ca293f6f989768fa9d92e73d910
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-05-17 09:52:39 -0700

    apple stocks notes

--- challenges/apple-stocks-notes.js
+++ challenges/apple-stocks-notes.js
@@ -13,38 +13,45 @@
  */
 
 // brute force, check every possibility (aka looking forward in time)
-// function bestProfit(arr) {
-
-// // iterate through array
-//   // for each value, find the largest value and memoize largest difference
-
-//   // we only need to check up to the 2nd-the-last value
-
-// }
-
 function bestProfit(arr) {
-  // Edge cases: Input not an array or empty array. Return 0.
   if (!Array.isArray(arr) || !arr.length) return 0;
+
+// iterate through array
+  // for each value, find the largest value and memoize largest difference
+  let largestProfit = -Infinity;
+
+  // we only need to check up to the 2nd-the-last value
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      const diff = arr[j] - arr[i];
+      if (diff > largestProfit) largestProfit = diff;
+    }
+  }
+  return largestProfit < 0 ? 0 : largestProfit;
+}
+
+function bestProfit(pricesArr) {
+  // Edge cases: Input not an array or empty array. Return 0.
+  if (!Array.isArray(pricesArr) || !pricesArr.length) return 0;
 
   // Use first price to initialize lowest price to buy.
   // Initialize highest profit as 0.
-  let cheapestPrice = arr[0];
-  let largestProfit = 0;
+  let cheapestPrice = pricesArr[0], profit = 0;
 
   // Loop through array.
-  for (let i = 0; i < arr.length; i += 1) {
+  for (let i = 0; i < pricesArr.length; i++) {
 
     // If we see a price lower than our lowest price so far,
     // set our lowest price so far to that lower price.
-    cheapestPrice = Math.min(cheapestPrice, arr[i]);
+    cheapestPrice = Math.min(cheapestPrice, pricesArr[i]);
 
     // Set profit to highest between itself and newest price
     // minus lowest price.
-    largestProfit = Math.max(largestProfit, arr[i] - cheapestPrice);
+    profit = Math.max(profit, pricesArr[i] - cheapestPrice);
   }
 
   // Return profit value at the end.
-  return largestProfit;
+  return profit;
 }
 
 console.log(bestProfit([0,1,6,-1,10,5])); // -> 11
