dca68abac36bf2978ddc7c6f2d7463e9478712ef
Mark Marcelo <markmarcelo@Marks-MBP.T-mobile.com>
2017-05-18 21:24:45 -0700

    missing num one-liner

--- challenges/missing-number.js
+++ challenges/missing-number.js
@@ -25,17 +25,30 @@ Challange:
   ** cannot use additional storage, variables are okay not any TYPE of object
   ** keep in mind time complexity
 */
-function missingNum(Array) {
-  let compare = 0;
-  let count = 1;
-  for (let i = 1; i < Array.length + 1; i += 1) {
-    compare = count ^ Array[i];
-    console.log(compare)
-    count += 1;
-  }
-  return compare;
-}
+// function missingNum(Array) {
+//   let compare = 0;
+//   let count = 1;
+//   for (let i = 1; i < Array.length + 1; i += 1) {
+//     compare = count ^ Array[i];
+//     console.log(compare)
+//     count += 1;
+//   }
+//   return compare;
+// }
 
-console.log(missingNum([2,3,1,5,6,7]));
+// // time complexity: O(n)
+// // space complexity: 1
+// function missingNum(array) {
+//   // get sum of all integers from 1 to (N+1) including missing element
+//   // using sum of linear series of number: n*(n+1)/2, n = Array.length + 1
+//   const sum = (array.length + 1) * (array.length + 2) / 2;
+
+//   // subtract all present integers from sum to find missing element
+//   return array.reduce((total, num) => total - num, sum);
+// };
+
+const missingNum = arr => arr.reduce((acc, num) => acc - num, (arr.length + 1) * (arr.length + 2) / 2);
+
+console.log(missingNum([2, 3, 1, 5, 6, 7]));
 
 module.exports = missingNum;
