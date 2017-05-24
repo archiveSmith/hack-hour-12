9c1e270cf2a3fd2547f4e785cd58437c1d39c105
Richard Ta <richardta@ucla.edu>
2017-05-23 23:26:25 -0700

    optimize sort algorithm

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,13 +3,17 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 function bubbleSort(array) {
+  let sorted;
   let arr = array.slice();
   for(let i = 0; i < array.length; i++) {
+    sorted = true;
     for(let j = 0; j < array.length - i; j++) {
       if(arr[j] > arr[j+1]) {
         swapValues(arr, j, j+1);
+        sorted = false;
       }
     }
+    if(sorted) return arr;
   }
   return arr;
 }
@@ -19,5 +23,7 @@ function swapValues(arr, idx1, idx2) {
   arr[idx1] = arr[idx2];
   arr[idx2] = temp;
 }
+
+const arr = [1,33,2,11,22,4,55,82,30];
 
 module.exports = bubbleSort;
