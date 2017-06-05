8d88d0111d7786356ea262b8a717dd3386c8eadf
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-06-05 12:08:50 -0700

    applyit approach notes

--- challenges/apply-it-approach.js
+++ challenges/apply-it-approach.js
@@ -25,9 +25,28 @@
  *  DO NOT USE THE BUILT IN APPLY METHOD OR THE SPREAD OPERATOR
  */
 
-// "Mark had an awesome solution that I added to the slides, check out how clean this is."
-// -George Anoberg
-// THANKS GEORGE!
+// create a function with 2 arguments: a function and an array
+// it will return a function
+// use "eval", template literals, and "join" convert the array to a string
 const applyIt = (func, args) => () => eval(`func("${args.join('","')}")`);
+
+
+// Test 1
+const jae = (name, age, location) => `${name} is ${age} and he lives in ${location}`;
+
+const jaero = applyIt(jae, ['Jae', 19, 'South Carolina']);
+
+// Test 2
+const jasmine = (name, age) => {
+  if (!age) return `We don't know how old ${name} is!`;
+  return `${name} is ${age} years old!`;
+};
+
+const jmoney = applyIt(jasmine, ['Jasmine']);
+
+
+// Log Tests
+console.log(jaero()); // Returns "Jae is 19 and he lives in South Carolina"
+console.log(jmoney()); // Returns "We don't know how old Jasmine is!"
 
 module.exports = applyIt;
