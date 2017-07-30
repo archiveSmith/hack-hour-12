933bd20f9e80640233154d55f58c7be3d5dc2c8d
Richard Ta <richardta@ucla.edu>
2017-07-29 19:35:58 -0700

    new solution with reduce

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,22 +13,34 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-  let result = [];
-  let bool;
-  for(let i = 0; i < array1.length; i++) {
-    bool = true;
-    if(!array2.includes(array1[i])) bool = false;
-    if(!array3.includes(array1[i])) bool = false;
-    if(!array4.includes(array1[i])) bool = false;
-    if(bool) result.push(array1[i]);
-  }
-  result = result.reduce((a, b) => {
-    if(a.indexOf(b) === -1) a.push(b);
-    return a;
-  },[]);
-  if(result.length === 0) return "Nothing in Common!";
-  return result;
+  return [...arguments].reduce((a, b) => {
+    let arr = [];
+    for(let i = 0; i < b.length; i++) {
+      if(a.includes(b[i]) && !arr.includes(b[i])) {
+        arr.push(b[i]);
+      }
+    }
+    return arr;
+  });
 }
+
+// function commonElements(array1, array2, array3, array4){
+//   let result = [];
+//   let bool;
+//   for(let i = 0; i < array1.length; i++) {
+//     bool = true;
+//     if(!array2.includes(array1[i])) bool = false;
+//     if(!array3.includes(array1[i])) bool = false;
+//     if(!array4.includes(array1[i])) bool = false;
+//     if(bool) result.push(array1[i]);
+//   }
+//   result = result.reduce((a, b) => {
+//     if(a.indexOf(b) === -1) a.push(b);
+//     return a;
+//   },[]);
+//   if(result.length === 0) return "Nothing in Common!";
+//   return result;
+// }
 
 // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
 // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
@@ -51,15 +63,14 @@ function commonElements(array1, array2, array3, array4){
 //   })
 // }
 
-function commmonElements(array1, array2, array3, array4) {
-  const set = new Set();
-  array1.forEach((item) => {
-    if(array2.includes(item) && array3.includes(item) && array4.includes(item)) {
-      set.add(item);
-    }
-  })
-  return set.size > 0 ? [...set] : 'Nothing in Common!';
-}
-
+// function commmonElements(array1, array2, array3, array4) {
+//   const set = new Set();
+//   array1.forEach((item) => {
+//     if(array2.includes(item) && array3.includes(item) && array4.includes(item)) {
+//       set.add(item);
+//     }
+//   })
+//   return set.size > 0 ? [...set] : 'Nothing in Common!';
+// }
 
 module.exports = commonElements;
